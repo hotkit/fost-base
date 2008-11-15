f0b1ebfe338a3c0c339d04f9988a7b2d40fa5bf1
Kirit Sælensminde <k@kirit.com>
2008-11-15 07:25:58 +0000

    Re-organised the parser headers a little so that we can add more parsers.

--- Cpp/include/fost/parse/json.hpp
+++ Cpp/include/fost/parse/json.hpp
@@ -6,54 +6,18 @@
 */
 
 
-#ifndef FOST_JSON_PARSE_HPP
-#define FOST_JSON_PARSE_HPP
+#ifndef FOST_PARSE_JSON_HPP
+#define FOST_PARSE_JSON_HPP
 
 
 #include <fost/json.hpp>
-#ifdef WIN32
-    #pragma warning ( disable : 4709 ) // comma operator within array index expression
-    #pragma warning ( disable : 4244 ) //conversion from 'int' to 'FSLib::utf16', possible loss of data
-#endif
-#include <boost/spirit/core.hpp>
-#include <boost/spirit/attribute.hpp>
-#include <boost/spirit/utility/confix.hpp>
-#include <boost/spirit/phoenix/primitives.hpp>
-#include <boost/spirit/phoenix/operators.hpp>
-#include <boost/spirit/phoenix/functions.hpp>
+#include <fost/parse/parse.hpp>
 
 
 namespace fostlib {
 
 
     namespace detail {
-
-        
-        // Implementation taken from
-        // http://spirit.sourceforge.net/distrib/spirit_1_8_5/libs/spirit/example/fundamental/stuff_vector.cpp
-        struct push_back_impl {
-            template <typename Container, typename Item>
-            struct result {
-                typedef void type;
-            };
-            template <typename Container, typename Item>
-            void operator()(Container& c, Item const& item) const {
-                c.push_back(item);
-            }
-        };
-        phoenix::function<push_back_impl> const push_back = push_back_impl();
-
-        struct insert_impl {
-            template <typename Container, typename Key, typename Value>
-            struct result {
-                typedef void type;
-            };
-            template <typename Container, typename Key, typename Value>
-            void operator()(Container& c, Key const& key, Value const& value) const {
-                c.insert( key, value );
-            }
-        };
-        phoenix::function<insert_impl> const insert = insert_impl();
 
 
         struct string_closure : boost::spirit::closure< string_closure, fostlib::string, std::vector< wchar_t >, wchar_t > {
@@ -71,7 +35,7 @@ namespace fostlib {
 
     }
 
-    
+
     extern const struct json_string_parser : public boost::spirit::grammar< json_string_parser, detail::string_closure::context_t > {
         template< typename scanner_t >
         struct definition {
@@ -166,4 +130,4 @@ namespace fostlib {
 }
 
 
-#endif // FOST_JSON_PARSE_HPP
+#endif // FOST_PARSE_JSON_HPP
---
+++ Cpp/include/fost/parse/parse.hpp
@@ -0,0 +1,64 @@
+/*
+    Copyright 2007-2008, Felspar Co Ltd. http://fost.3.felspar.com/
+    Distributed under the Boost Software License, Version 1.0.
+    See accompanying file LICENSE_1_0.txt or copy at
+        http://www.boost.org/LICENSE_1_0.txt
+*/
+
+
+#ifndef FOST_PARSE_PARSE_HPP
+#define FOST_PARSE_PARSE_HPP
+
+
+#ifdef WIN32
+    #pragma warning ( disable : 4709 ) // comma operator within array index expression
+    #pragma warning ( disable : 4244 ) //conversion from 'int' to 'FSLib::utf16', possible loss of data
+#endif
+#include <boost/spirit/core.hpp>
+#include <boost/spirit/attribute.hpp>
+#include <boost/spirit/utility/confix.hpp>
+#include <boost/spirit/phoenix/primitives.hpp>
+#include <boost/spirit/phoenix/operators.hpp>
+#include <boost/spirit/phoenix/functions.hpp>
+
+
+namespace fostlib {
+
+
+    namespace detail {
+
+
+        // Implementation taken from
+        // http://spirit.sourceforge.net/distrib/spirit_1_8_5/libs/spirit/example/fundamental/stuff_vector.cpp
+        struct push_back_impl {
+            template <typename Container, typename Item>
+            struct result {
+                typedef void type;
+            };
+            template <typename Container, typename Item>
+            void operator()(Container& c, Item const& item) const {
+                c.push_back(item);
+            }
+        };
+        phoenix::function<push_back_impl> const push_back = push_back_impl();
+
+        struct insert_impl {
+            template <typename Container, typename Key, typename Value>
+            struct result {
+                typedef void type;
+            };
+            template <typename Container, typename Key, typename Value>
+            void operator()(Container& c, Key const& key, Value const& value) const {
+                c.insert( key, value );
+            }
+        };
+        phoenix::function<insert_impl> const insert = insert_impl();
+
+
+    }
+
+
+}
+
+
+#endif // FOST_PARSE_PARSE_HPP
