e2a34fca094a292eeceed02f2415e5ff5692a27a
Kirit Sælensminde <k@kirit.com>
2008-11-15 05:26:05 +0000

    Added 8 bit integer types for MSVC.

--- Cpp/include/fost/config.hpp
+++ Cpp/include/fost/config.hpp
@@ -30,6 +30,8 @@
     #endif
 
     // In stdint.h on Linux
+    typedef __int8 int8_t;
+    typedef unsigned __int8 uint8_t;
     typedef __int16 int16_t;
     typedef unsigned __int16 uint16_t;
     typedef __int32 int32_t;
