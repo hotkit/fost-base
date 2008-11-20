2ce08841cc5e438117b9a96342fac9e5556938d9
Kirit Sælensminde <k@kirit.com>
2008-11-20 07:36:30 +0000

    Added first Project Euler example.

---
+++ Examples/Project Euler/014/main.cpp
@@ -0,0 +1,19 @@
+/*
+    Copyright 2008, Felspar Co Ltd. http://fost.3.felspar.com/
+    Distributed under the Boost Software License, Version 1.0.
+    See accompanying file LICENSE_1_0.txt or copy at
+        http://www.boost.org/LICENSE_1_0.txt
+*/
+
+
+#include <fost/cli>
+#include <fost/main.hpp>
+
+
+FSL_MAIN(
+    L"014",
+    L"Project Euler puzzle 14"
+)( fostlib::ostream &, fostlib::arguments & ) {
+    std::map< std::size_t, std::vector< unsigned int > > cache;
+    return 0;
+}
