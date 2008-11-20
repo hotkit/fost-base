865db3328f8428462b6a93e50a4d099df345a219
Kirit Sælensminde <k@kirit.com>
2008-11-20 09:15:21 +0000

    Produces the correct result in just over 1s on my machine.

--- Examples/Project Euler/014/main.cpp
+++ Examples/Project Euler/014/main.cpp
@@ -8,12 +8,49 @@
 
 #include <fost/cli>
 #include <fost/main.hpp>
+#include <iostream>
+
+
+using namespace fostlib;
+
+typedef unsigned int int_type;
+
+int_type next_n( int_type n ) {
+    if ( n % 2 )
+        return 3 * n + 1;
+    else
+        return n / 2;
+}
+
+int_type chain_length( int_type n ) {
+    if ( n == 1 )
+        return 1;
+    typedef std::vector< std::size_t > vector_type;
+    static std::map< std::size_t, vector_type > cache;
+    const std::size_t divisor = 1000;
+    vector_type &cache_line( cache[ n / divisor ] );
+    if ( cache_line.empty() ) {
+        //std::cout << "Empty cache line for " << ( n / divisor ) << std::endl;
+        cache_line = vector_type( divisor );
+    }
+    vector_type::value_type &item = cache_line[ n % divisor ];
+    if ( item == 0 )
+        item = 1 + chain_length( next_n( n ) );
+    return item;
+}
 
 
 FSL_MAIN(
-    L"014",
+    L"p014",
     L"Project Euler puzzle 14"
 )( fostlib::ostream &, fostlib::arguments & ) {
-    std::map< std::size_t, std::vector< unsigned int > > cache;
+    std::pair< int_type, std::size_t > longest( 0, 0 );
+    for ( int_type n = 1; n != 1000000; ++n ) {
+        std::size_t length = chain_length( n );
+        if ( length > longest.second ) {
+            longest = std::make_pair( n, length );
+            std::cout << longest.first << " has length " << longest.second << std::endl;
+        }
+    }
     return 0;
 }
