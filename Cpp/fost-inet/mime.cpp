db31a52f6befe2d5a23722b3cbd9de65077b9914
Kirit Sælensminde <k@kirit.com>
2008-11-29 06:57:38 +0000

    Header file for mime headers now brought across from FOST.3.

---
+++ Cpp/fost-inet/mime.cpp
@@ -0,0 +1,14 @@
+/*
+    Copyright 1999-2008, Felspar Co Ltd. http://fost.3.felspar.com/
+    Distributed under the Boost Software License, Version 1.0.
+    See accompanying file LICENSE_1_0.txt or copy at
+        http://www.boost.org/LICENSE_1_0.txt
+*/
+
+
+#include "fost-inet.hpp"
+#include <fost/internet/mime.hpp>
+
+
+using namespace fostlib;
+
---
+++ Cpp/include/fost/internet/mime.hpp
@@ -0,0 +1,95 @@
+/*
+    Copyright 1999-2008, Felspar Co Ltd. http://fost.3.felspar.com/
+    Distributed under the Boost Software License, Version 1.0.
+    See accompanying file LICENSE_1_0.txt or copy at
+        http://www.boost.org/LICENSE_1_0.txt
+*/
+
+
+#ifndef FOST_INTERNET_MIME_HPP
+#define FOST_INTERNET_MIME_HPP
+#pragma once
+
+
+#include <fost/core>
+
+
+namespace fostlib {
+
+
+    class FOST_INET_DECLSPEC FSL_ABSTRACT headers_base {
+        headers_base();
+    public:
+        class content;
+
+        void parse( const string &headers );
+
+        bool exists( const string & ) const;
+        content &add( const string &name, const content & );
+        const content &operator[]( const string & ) const;
+
+        typedef std::map< string, content >::const_iterator const_iterator;
+        const_iterator begin() const;
+        const_iterator end() const;
+
+        class FOST_INET_DECLSPEC content {
+        public:
+            content();
+            content( wliteral );
+            content( const string & );
+            content( const string &, const std::map< string, string > & );
+
+            accessors< string > value;
+
+            content &subvalue( const string &k, const string &v );
+            nullable< string > subvalue( const string &k ) const;
+
+            typedef std::map< string, string >::const_iterator const_iterator;
+            const_iterator begin() const;
+            const_iterator end() const;
+
+        private:
+            std::map< string, string > m_subvalues;
+        };
+
+    protected:
+        virtual std::pair< string, content > value( const string &name, const string &value ) = 0;
+
+    private:
+        std::map< fostlib::string, content > m_headers;
+    };
+
+
+    /*
+        This initial implementation is only intended to be able to parse HTTP POST multipart/form-data encoded data
+    */
+    class FOST_INET_DECLSPEC mime : boost::noncopyable {
+    public:
+        class FOST_INET_DECLSPEC mime_headers : public fostlib::headers_base {
+        protected:
+            std::pair< string, headers_base::content > value( const string &name, const string &value );
+        };
+
+        mime( const char *content_type = "multipart/mixed" );
+        mime( const mime_headers &headers, const char *content_type = "multipart/mixed" );
+        virtual ~mime();
+
+        accessors< string > content_type;
+        accessors< mime_headers, fostlib::lvalue > headers;
+        accessors< std::list< boost::shared_ptr< mime > >, fostlib::lvalue > items;
+
+        virtual std::ostream &print_on( std::ostream & ) const;
+
+        virtual bool boundary_is_ok( const string &boundary ) const;
+    };
+
+
+    FOST_INET_DECLSPEC std::ostream &operator <<( std::ostream &o, const headers_base &headers );
+    FOST_INET_DECLSPEC std::ostream &operator <<( std::ostream &o, const headers_base::content &value );
+    FOST_INET_DECLSPEC std::ostream &operator <<( std::ostream &o, const mime & );
+
+
+}
+
+
+#endif // FOST_INTERNET_MIME_HPP
