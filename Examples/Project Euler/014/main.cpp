1460df8c5fb52824143c295d4c28ea4e808179bd
Kirit Sælensminde <k@kirit.com>
2008-11-30 09:55:21 +0000

    A simpler version that doesn't memoize.

--- Examples/Project Euler/014/main.cpp
+++ Examples/Project Euler/014/main.cpp
@@ -8,7 +8,6 @@
 
 #include <fost/cli>
 #include <fost/main.hpp>
-#include <iostream>
 
 
 using namespace fostlib;
@@ -25,31 +24,20 @@ int_type next_n( int_type n ) {
 int_type chain_length( int_type n ) {
     if ( n == 1 )
         return 1;
-    typedef std::vector< std::size_t > vector_type;
-    static std::map< std::size_t, vector_type > cache;
-    const std::size_t divisor = 1000;
-    vector_type &cache_line( cache[ n / divisor ] );
-    if ( cache_line.empty() ) {
-        //std::cout << "Empty cache line for " << ( n / divisor ) << std::endl;
-        cache_line = vector_type( divisor );
-    }
-    vector_type::value_type &item = cache_line[ n % divisor ];
-    if ( item == 0 )
-        item = 1 + chain_length( next_n( n ) );
-    return item;
+    else
+        return 1 + chain_length( next_n( n ) );
 }
-
 
 FSL_MAIN(
     L"p014",
     L"Project Euler puzzle 14"
-)( fostlib::ostream &, fostlib::arguments & ) {
+)( fostlib::ostream &cout, fostlib::arguments & ) {
     std::pair< int_type, std::size_t > longest( 0, 0 );
-    for ( int_type n = 1; n != 1000000; ++n ) {
+    for ( int_type n = 500001; n <= 1000000; n += 2 ) {
         std::size_t length = chain_length( n );
         if ( length > longest.second ) {
             longest = std::make_pair( n, length );
-            std::cout << longest.first << " has length " << longest.second << std::endl;
+            cout << longest.first << " has length " << longest.second << std::endl;
         }
     }
     return 0;
