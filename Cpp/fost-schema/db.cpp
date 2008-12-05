392063ffa651702eba2bf860bd106e6f4f0ab32b
Kirit Sælensminde <k@kirit.com>
2008-12-05 12:05:38 +0000

    Moved exceptions from db.cpp.

--- Cpp/fost-schema/db.cpp
+++ Cpp/fost-schema/db.cpp
@@ -70,27 +70,6 @@ namespace {
 }
 
 
-fostlib::exceptions::data_driver::data_driver( const string&m, const string &d ) throw ()
-: exception( m ) {
-    m_info << L"Driver: " << d << std::endl;
-}
-fostlib::exceptions::data_driver::data_driver( const string &m, const string &d1, const string &d2 ) throw ()
-: exception( m ) {
-    m_info << L"Driver 1: " << d1 << L"\nDriver 2: " << d2 << std::endl;
-}
-wliteral const fostlib::exceptions::data_driver::message() const throw () {
-    return L"Problem with database driver.";
-}
-
-
-fostlib::exceptions::transaction_fault::transaction_fault( const string &error ) throw ()
-: exception( error ) {
-}
-wliteral const fostlib::exceptions::transaction_fault::message() const throw () {
-    return L"Fault in transaction handling.";
-}
-
-
 /*
     DBInterface
 */
--- Cpp/fost-schema/exception.cpp
+++ Cpp/fost-schema/exception.cpp
@@ -1,5 +1,5 @@
 /*
-    Copyright 2008, Felspar Co Ltd. http://fost.3.felspar.com/
+    Copyright 1999-2008, Felspar Co Ltd. http://fost.3.felspar.com/
     Distributed under the Boost Software License, Version 1.0.
     See accompanying file LICENSE_1_0.txt or copy at
         http://www.boost.org/LICENSE_1_0.txt
@@ -7,6 +7,20 @@
 
 
 #include "fost-schema.hpp"
+#include <fost/db.hpp>
+
+
+fostlib::exceptions::data_driver::data_driver( const string&m, const string &d ) throw ()
+: exception( m ) {
+    m_info << L"Driver: " << d << std::endl;
+}
+fostlib::exceptions::data_driver::data_driver( const string &m, const string &d1, const string &d2 ) throw ()
+: exception( m ) {
+    m_info << L"Driver 1: " << d1 << L"\nDriver 2: " << d2 << std::endl;
+}
+fostlib::wliteral const fostlib::exceptions::data_driver::message() const throw () {
+    return L"Problem with database driver.";
+}
 
 
 #include <fost/exception/no_attribute.hpp>
@@ -20,5 +34,13 @@ fostlib::exceptions::no_attribute::no_attribute( const string &attribute ) throw
 }
 fostlib::wliteral const fostlib::exceptions::no_attribute::message() const throw () {
     return L"Attribute could not be found on the instance";
+}
+
+
+fostlib::exceptions::transaction_fault::transaction_fault( const string &error ) throw ()
+: exception( error ) {
+}
+fostlib::wliteral const fostlib::exceptions::transaction_fault::message() const throw () {
+    return L"Fault in transaction handling.";
 }
 
