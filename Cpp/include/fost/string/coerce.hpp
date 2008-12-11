0a0d8af2357b4df98dc0bd423dccba561e81aa99
Kirit Sælensminde <k@kirit.com>
2008-12-11 08:58:44 +0000

    Added coercion for unsigned long to string on platforms that use long.

--- Cpp/include/fost/string/coerce.hpp
+++ Cpp/include/fost/string/coerce.hpp
@@ -47,6 +47,13 @@ namespace fostlib {
         string coerce( unsigned int sz );
     };
 
+#ifdef FOST_USE_LONG
+    template<>
+    struct FOST_CORE_DECLSPEC coercer< string, unsigned long > {
+        string coerce( unsigned long ul );
+    };
+#endif
+
     template<>
     struct FOST_CORE_DECLSPEC coercer< int64_t, string > {
         int64_t coerce( const string &str );
