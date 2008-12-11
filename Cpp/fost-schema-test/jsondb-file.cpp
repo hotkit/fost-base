03ca583f8653cbd500aeb38565957ea0bdc4dbf1
Kirit Sælensminde <k@kirit.com>
2008-12-11 13:15:25 +0000

    Added a test program that will test the persistence (durability) of the JSON databases.

---
+++ Cpp/fost-schema-test/jsondb-file.cpp
@@ -0,0 +1,20 @@
+/*
+    Copyright 2005-2008, Felspar Co Ltd. http://fost.3.felspar.com/
+    Distributed under the Boost Software License, Version 1.0.
+    See accompanying file LICENSE_1_0.txt or copy at
+        http://www.boost.org/LICENSE_1_0.txt
+*/
+
+
+#include <fost/core>
+#include <fost/cli>
+#include <fost/main.hpp>
+
+
+FSL_MAIN(
+    L"fost-schema-test-jsondb-file",
+    L"fost-schema-test-jsondb-file\nTest the file handling for Fost 4 JSON databases\nCopyright (c) 2008, Felspar Co. Ltd."
+)( fostlib::ostream &out, fostlib::arguments &args ) {
+    return 0;
+}
+
