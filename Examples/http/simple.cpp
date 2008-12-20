c453be6a33384e490d7e0917da67436a46524f04
Kirit Sælensminde <k@kirit.com>
2008-12-20 06:02:09 +0000

    The port number is now a setting.

--- Examples/http/simple.cpp
+++ Examples/http/simple.cpp
@@ -17,6 +17,7 @@ using namespace fostlib;
 
 namespace {
     setting< string > c_host( L"http-simple", L"Server", L"Bind to", L"localhost" );
+    setting< int > c_port( L"http-simple", L"Server", L"Port", 8001 );
 }
 
 
@@ -24,12 +25,12 @@ FSL_MAIN(
     L"http-simple",
     L"Simple HTTP server\nCopyright (c) 2008, Felspar Co. Ltd."
 )( fostlib::ostream &o, fostlib::arguments & ) {
-    http::server server( host( c_host.value() ), 8001 );
+    http::server server( host( c_host.value() ), c_port.value() );
     o << L"Answering requests on http://" << server.binding() << L":" << server.port() << L"/" << std::endl;
     for ( bool process( true ); process; ) {
         std::auto_ptr< http::request > req( server() );
         o << req->method() << L" " << req->file_spec() << std::endl;
-        (*req)( text_body( L"This is a response" ) );
+        (*req)( text_body( L"<html><body>This <b>is</b> a response</body></html>", L"text/html" ) );
         process = false;
     };
     o << L"Told to exit" << std::endl;
