cbcdc990b5fbc9c2b2bb8771bd7b4bbcef4e440e
Kirit Sælensminde <k@kirit.com>
2008-12-20 06:35:06 +0000

    Removed parts that are now in a common header.

--- Cpp/include/fost/internet/http.server.hpp
+++ Cpp/include/fost/internet/http.server.hpp
@@ -11,29 +11,13 @@
 #pragma once
 
 
-#include <fost/internet/url.hpp>
-#include <fost/internet/mime.hpp>
+#include <fost/internet/http.hpp>
 
 
 namespace fostlib {
 
 
     namespace http {
-
-
-        class request : boost::noncopyable {
-        public:
-            request( std::auto_ptr< boost::asio::ip::tcp::socket > socket );
-
-            const string &method();
-            const string &file_spec();
-
-            void operator() ( const mime &response );
-
-        private:
-            std::auto_ptr< boost::asio::ip::tcp::socket > m_sock;
-            nullable< std::pair< string, string > > m_first_line;
-        };
 
 
         class server : boost::noncopyable {
