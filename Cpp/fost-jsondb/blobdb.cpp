1eb9b3ecfaa8869ca560e6c378e89802594f3cd3
Kirit Sælensminde <k@kirit.com>
2008-12-25 05:03:23 +0000

    Started to split out the low level JSON "blob" database from the O/RM schema functionality.

---
+++ Cpp/fost-jsondb/blobdb.cpp
@@ -0,0 +1,19 @@
+/*
+    Copyright 1999-2008, Felspar Co Ltd. http://fost.3.felspar.com/
+    Distributed under the Boost Software License, Version 1.0.
+    See accompanying file LICENSE_1_0.txt or copy at
+        http://www.boost.org/LICENSE_1_0.txt
+*/
+
+
+#include "fost-jsondb.hpp"
+#include <fost/jsondb.hpp>
+#include <fost/unicode.hpp>
+
+#include <fost/exception/not_null.hpp>
+#include <fost/exception/out_of_range.hpp>
+#include <fost/exception/unexpected_eof.hpp>
+
+
+using namespace fostlib;
+
---
+++ Cpp/include/fost/jsondb.hpp
@@ -0,0 +1,30 @@
+/*
+    Copyright 2007-2008, Felspar Co Ltd. http://fost.3.felspar.com/
+    Distributed under the Boost Software License, Version 1.0.
+    See accompanying file LICENSE_1_0.txt or copy at
+        http://www.boost.org/LICENSE_1_0.txt
+*/
+
+
+#ifndef FOST_JSONDB_HPP
+#define FOST_JSONDB_HPP
+#pragma once
+
+
+#include <fost/core>
+#include <fost/json.hpp>
+#include <fost/thread.hpp>
+
+
+namespace fostlib {
+
+
+    class FOST_JSONDB_DECLSPEC jsondb {
+
+    };
+
+
+}
+
+
+#endif // FOST_JSONDB_HPP
