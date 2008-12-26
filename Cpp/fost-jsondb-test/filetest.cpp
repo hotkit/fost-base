2c8018018a1527c6d2163d54155a2b07acec25dd
Kirit Sælensminde <k@kirit.com>
2008-12-26 07:18:34 +0000

    Got the right starting point for the file testing.

--- Cpp/fost-jsondb-test/filetest.cpp
+++ Cpp/fost-jsondb-test/filetest.cpp
@@ -24,11 +24,7 @@ FSL_MAIN(
     L"fost-jsondb-test-file",
     L"fost-jsondb-test-file\nTest the file handling for Fost 4 JSON blobs\nCopyright (c) 2008, Felspar Co. Ltd."
 )( fostlib::ostream &out, fostlib::arguments &args ) {
-    if ( args[ 1 ].isnull() ) {
-        out << L"Supply a JSON filename as the first parameter" << std::endl;
-        return 1;
-    }
+    string dbname( guid() );
 
     return 0;
 }
-
