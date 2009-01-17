37547c1091d04c4aaaac7c7a76802c0dd766cd35
Kirit Sælensminde <k@kirit.com>
2009-01-17 06:22:29 +0000

    Added new examples area for BeerCamp puzzles.

---
+++ Examples/BeerCamp/Primes/primes.cpp
@@ -0,0 +1,18 @@
+/*
+    Copyright 2009, Felspar Co Ltd. http://fost.3.felspar.com/
+    Distributed under the Boost Software License, Version 1.0.
+    See accompanying file LICENSE_1_0.txt or copy at
+        http://www.boost.org/LICENSE_1_0.txt
+*/
+
+
+#include <fost/cli>
+#include <fost/main.hpp>
+
+
+FSL_MAIN(
+    L"primes",
+    L"Primes\nCopyright (c) 2009 Felspar Co. Ltd."
+)( fostlib::ostream &out, fostlib::arguments &args ) {
+    return 0;
+}
