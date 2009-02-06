13b51383f7800a506e5ff2a3276338c2f68bd291
Kirit Sælensminde <k@kirit.com>
2009-02-06 03:18:24 +0000

    Complete solution to the problem.

--- Examples/BeerCamp/Primes/primes.cpp
+++ Examples/BeerCamp/Primes/primes.cpp
@@ -9,40 +9,76 @@
 #include <fost/cli>
 #include <fost/main.hpp>
 #include <boost/bind.hpp>
+#include <boost/lambda/lambda.hpp>
+#include <boost/lambda/bind.hpp>
+#include <numeric>
 
 
 namespace {
-    bool is_prime( int v ) {
-        if ( v < 4 )
-            return true;
-        else if ( v % 2 == 0 )
-            return false;
+    bool is_prime( unsigned int v ) {
+        if ( v % 2 == 0 )
+            return v == 2;
         else {
-            for ( int i = 3; i < v; i += 2 )
+            unsigned int u = std::sqrt( v );
+            for ( unsigned int i = 3; i <= u; i += 2 )
                 if ( v % i == 0 )
                     return false;
             return true;
         }
     }
 
-    int next_prime( int &base ) {
+    unsigned int next_prime( unsigned int &base ) {
         while ( !is_prime( base++ ) )
             ;
         return base - 1;
     }
+
+    struct prime_sequence {
+        prime_sequence( std::size_t length )
+        : primes( length ), generator( boost::bind( next_prime, 2U ) ) {
+            std::generate( primes.begin(), primes.end(), generator );
+            sum( std::accumulate( primes.begin(), primes.end(), 0 ) );
+            if ( !is_prime( sum() ) )
+                ++(*this);
+        }
+
+        fostlib::accessors< unsigned int > sum;
+
+        void shift() {
+            std::copy( ++primes.begin(), primes.end(), primes.begin() );
+            (*primes.rbegin()) = generator();
+            sum( std::accumulate( primes.begin(), primes.end(), 0 ) );
+        }
+        prime_sequence &operator ++ () {
+            do shift(); while ( !is_prime( sum() ) );
+            return *this;
+        }
+    private:
+        std::vector< unsigned int > primes;
+        boost::function< unsigned int ( void ) > generator;
+    };
+
 }
 
-boost::function< int ( void ) > prime_generator() {
-    return boost::bind( next_prime, 2 );
-}
 
 FSL_MAIN(
     L"primes",
     L"Primes\nCopyright (c) 2009 Felspar Co. Ltd."
 )( fostlib::ostream &out, fostlib::arguments &args ) {
-    boost::function< int ( void ) > generator = prime_generator();
-    for ( int i = 0; i < 453; ++i )
-        out << generator() << L" ";
-    out << std::endl;
+    std::list< prime_sequence > seqs;
+    for ( std::size_t c( 1 ); c != args.size(); ++c )
+        seqs.push_back( prime_sequence( fostlib::coerce< int >( args[ c ].value() ) ) );
+    std::size_t matches;
+    do {
+        matches = 0;
+        for ( std::list< prime_sequence >::iterator p( seqs.begin() ), n( ++seqs.begin() ); n != seqs.end(); ++p, ++n )
+            if ( p->sum() < n->sum() )
+                ++(*p);
+            else if ( n->sum() < p->sum() )
+                ++(*n);
+            else
+                ++matches;
+    } while ( matches < seqs.size() - 1 );
+    out << seqs.begin()->sum() << std::endl;
     return 0;
 }
