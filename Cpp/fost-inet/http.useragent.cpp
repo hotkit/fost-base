16d9b3d27bd890ba87626c3fc4a197bb5e03ce77
Kirit Sælensminde <k@kirit.com>
2009-03-21 08:55:26 +0000

    Tidied up some of the implementation of the HTTP user agent. The request and response sides of the interaction are now a little more cleanly separated.

--- Cpp/fost-inet/http.useragent.cpp
+++ Cpp/fost-inet/http.useragent.cpp
@@ -1,5 +1,5 @@
 /*
-    Copyright 2008, Felspar Co Ltd. http://fost.3.felspar.com/
+    Copyright 2008-2009, Felspar Co Ltd. http://fost.3.felspar.com/
     Distributed under the Boost Software License, Version 1.0.
     See accompanying file LICENSE_1_0.txt or copy at
         http://www.boost.org/LICENSE_1_0.txt
@@ -20,12 +20,24 @@ using namespace fostlib;
 fostlib::http::user_agent::user_agent() {
 }
 
-std::auto_ptr< http::user_agent::response > fostlib::http::user_agent::operator()(
+std::auto_ptr< http::user_agent::response > fostlib::http::user_agent::operator () (
     const string &method, const url &url, const nullable< string > &data
 ) {
     std::auto_ptr< asio::tcpsocket > socket( new asio::tcpsocket( m_service ) );
     socket->socket.connect( boost::asio::ip::tcp::endpoint( url.server().address(), url.port().value( 80 ) ) );
-    return std::auto_ptr< http::user_agent::response >( new http::user_agent::response( socket, method, url, data ) );
+
+    asio::send( *socket, coerce< utf8string >( method ) + " " + coerce< utf8string >( url.pathspec() ) + " HTTP/1.1\r\n" );
+
+    text_body request( data.value( string() ) );
+    request.headers().add( L"Host", url.server().name() );
+    std::stringstream ss;
+    request.print_on( ss );
+    asio::send( *socket, ss.str() );
+
+    if ( !data.isnull() )
+        asio::send( *socket, data.value().std_str() );
+
+    return std::auto_ptr< http::user_agent::response >( new http::user_agent::response( socket, method, url ) );
 }
 
 
@@ -35,16 +47,8 @@ std::auto_ptr< http::user_agent::response > fostlib::http::user_agent::operator(
 
 fostlib::http::user_agent::response::response(
     std::auto_ptr< asio::tcpsocket > sock,
-    const string &method, const url &url, const nullable< string > &data
-) : method( method ), location( url ), data( data ), m_socket( sock ) {
-    asio::send( *m_socket, coerce< utf8string >( method ) + " " + coerce< utf8string >( url.pathspec() ) + " HTTP/1.1\r\n" );
-
-    text_body request( data.value( string() ) );
-    request.headers().add( L"Host", url.server().name() );
-    std::stringstream ss;
-    request.print_on( ss );
-    asio::send( *m_socket, ss.str() );
-
+    const string &method, const url &url
+) : method( method ), location( url ), m_socket( sock ) {
     string first_line;
     asio::getline( *m_socket, first_line, L"\r\n" );
 
@@ -55,4 +59,5 @@ fostlib::http::user_agent::response::response(
             break;
         headers().parse( line );
     }
+    content_type( headers()[ L"Content-Type" ].value() );
 }
--- Examples/http/fget.cpp
+++ Examples/http/fget.cpp
@@ -1,5 +1,5 @@
 /*
-    Copyright 2008, Felspar Co Ltd. http://fost.3.felspar.com/
+    Copyright 2008-2009, Felspar Co Ltd. http://fost.3.felspar.com/
     Distributed under the Boost Software License, Version 1.0.
     See accompanying file LICENSE_1_0.txt or copy at
         http://www.boost.org/LICENSE_1_0.txt
@@ -17,11 +17,19 @@ using namespace fostlib;
 
 FSL_MAIN(
     L"fget",
-    L"Simple HTTP client\nCopyright (c) 2008, Felspar Co. Ltd."
+    L"Simple HTTP client\nCopyright (C) 2008-2009, Felspar Co. Ltd."
 )( fostlib::ostream &o, fostlib::arguments &args ) {
+    // The URL to be fetched (default to localhost)
     string location = args[ 1 ].value( L"http://localhost/" );
     o << location << std::endl;
+    // Create a user agent and request the URL
     http::user_agent browser;
     std::auto_ptr< http::user_agent::response > response( browser.get( url( location ) ) );
+    o << response->headers() << std::endl;
+    // If the body is HTML then display it
+    if ( response->content_type().substr( 0, 5 ) == L"text/" )
+        o << response->items().size() << L" items" << std::endl;
+    else
+        o << L"Not text : " << response->content_type() << std::endl;
     return 0;
 }
