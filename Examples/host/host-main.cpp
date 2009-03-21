7f9384b7e3401ad30a6bb24e57fe8c79ba7cf1f5
Kirit Sælensminde <k@kirit.com>
2009-03-21 07:00:59 +0000

    Updated the host example to rename it and make it install to the dist/bin folder like other executables. Also worked out how the <dll-path> directive works.

--- Examples/host/host-main.cpp
+++ Examples/host/host-main.cpp
@@ -1,5 +1,5 @@
 /*
-    Copyright 2008, Felspar Co Ltd. http://fost.3.felspar.com/
+    Copyright 2008-2009, Felspar Co Ltd. http://fost.3.felspar.com/
     Distributed under the Boost Software License, Version 1.0.
     See accompanying file LICENSE_1_0.txt or copy at
         http://www.boost.org/LICENSE_1_0.txt
@@ -15,8 +15,8 @@ using namespace fostlib;
 
 
 FSL_MAIN(
-    L"fhost",
-    L"fhost\nCopyright (c) 2008 Felspar Co. Ltd."
+    L"host",
+    L"host\nCopyright (C) 2008-2009 Felspar Co. Ltd."
 )( ostream &out, arguments &args ) {
     for ( arguments::size_type c( 1 ); c < args.size(); ++c )
         try {
