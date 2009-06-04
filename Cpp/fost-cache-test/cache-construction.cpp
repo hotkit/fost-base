ee3dc8445a8d5d5e8b9d763d8e9bccf00564f20d
Kirit Sælensminde <k@kirit.com>
2009-06-04 11:35:01 +0000

    Added skeleton for the fost-cache tests.

---
+++ Cpp/fost-cache-test/cache-construction.cpp
@@ -0,0 +1,13 @@
+/*
+    Copyright 2009, Felspar Co Ltd. http://fost.3.felspar.com/
+    Distributed under the Boost Software License, Version 1.0.
+    See accompanying file LICENSE_1_0.txt or copy at
+        http://www.boost.org/LICENSE_1_0.txt
+*/
+
+
+#include "fost-cache-test.hpp"
+
+
+FSL_TEST_SUITE( construction );
+
