510e7c92a932f46620b2fec25c11b511587d92a7
Kirit Sælensminde <k@kirit.com>
2009-06-14 05:06:13 +0000

    The Postgres database driver now compiles on Windows. We can't run the tests yet without sorting out a mechanism that will allow the correct driver to be loaded if it's not already present.

--- Cpp/include/fost/config.hpp
+++ Cpp/include/fost/config.hpp
@@ -1,5 +1,5 @@
 /*
-    Copyright 2001-2008, Felspar Co Ltd. http://fost.3.felspar.com/
+    Copyright 2001-2009, Felspar Co Ltd. http://fost.3.felspar.com/
     Distributed under the Boost Software License, Version 1.0.
     See accompanying file LICENSE_1_0.txt or copy at
         http://www.boost.org/LICENSE_1_0.txt
@@ -40,12 +40,14 @@
     typedef unsigned __int64 uint64_t;
 
     #define FSL_ABSTRACT __declspec( novtable )
+    #define FSL_EXPORT __declspec( dllexport )
 #else
     #include <stdint.h>
 
     typedef long LONG;
 
     #define FSL_ABSTRACT
+    #define FSL_EXPORT
 
     #undef Null
 #endif
