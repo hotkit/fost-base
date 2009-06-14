c2389e9cd7dabc81a56a7df00ad1d9b5579bfd8a
Kirit Sælensminde <k@kirit.com>
2009-06-14 03:32:44 +0000

    Removed tests for the libraries now moved to fost-orm.

--- compile.cmd
+++ compile.cmd
@@ -2,15 +2,11 @@
 
 ..\bjam toolset=msvc %*
 
+call ..\boost-version.cmd
 IF NOT ERRORLEVEL 1 (
-    ..\dist\bin\ftest ..\dist\bin\fost-core-test-smoke.dll ..\dist\bin\fost-crypto-test-smoke.dll ..\dist\bin\fost-inet-test-smoke.dll  ..\dist\bin\fost-jsondb-test-smoke.dll ..\dist\bin\fost-schema-test-smoke.dll
+    for /r %BUILD_DIRECTORY% %%f in (*.pdb) do xcopy /D /Y %%f ..\dist\bin
 )
 
 IF NOT ERRORLEVEL 1 (
-    ..\dist\bin\fost-schema-test-jsondb-file Cpp\fost-schema-test\jsondb-file.json
+    ..\dist\bin\ftest ..\dist\bin\fost-core-test-smoke.dll ..\dist\bin\fost-crypto-test-smoke.dll ..\dist\bin\fost-inet-test-smoke.dll
 )
-
-IF NOT ERRORLEVEL 1 (
-    ..\dist\bin\fost-jsondb-test-file
-)
-
