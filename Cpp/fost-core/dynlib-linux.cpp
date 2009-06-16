91a91e3218ba5c389aaa55b020561baa3ee89a7f
Kirit Sælensminde <k@kirit.com>
2009-06-16 06:58:46 +0000

    Added some commented out instrumentation to show when libraries are loaded in and out.

--- Cpp/fost-core/dynlib-linux.cpp
+++ Cpp/fost-core/dynlib-linux.cpp
@@ -1,5 +1,5 @@
 /*
-    Copyright 2008, Felspar Co Ltd. http://fost.3.felspar.com/
+    Copyright 2008-2009, Felspar Co Ltd. http://fost.3.felspar.com/
     Distributed under the Boost Software License, Version 1.0.
     See accompanying file LICENSE_1_0.txt or copy at
         http://www.boost.org/LICENSE_1_0.txt
@@ -7,11 +7,14 @@
 
 
 #include <dlfcn.h>
+#include <iostream>
+
 
 using namespace fostlib;
 
 
 struct fostlib::dynlib::impl {
+    string name;
     void *handle;
 };
 
@@ -20,6 +23,8 @@ fostlib::dynlib::dynlib( const string &lib )
 : m_lib( new impl ) {
     if ( ( m_lib->handle = dlopen( lib.c_str(), RTLD_NOW ) ) == NULL )
         throw fostlib::exceptions::null( L"dlopen failed for " + lib, string( dlerror() ) );
+    m_lib->name = lib;
+    //std::cout << L"Loaded " << lib << std::endl;
 }
 
 
@@ -27,6 +32,7 @@ fostlib::dynlib::~dynlib() {
     if ( m_lib ) {
         if ( m_lib->handle )
             dlclose( m_lib->handle );
+        //std::cout << L"Unloaded " << m_lib->name << std::endl;
         delete m_lib;
     }
 }
