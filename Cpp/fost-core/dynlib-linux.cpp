702bbc35abf22a8e91820822f0c8e2537cdd1790
Kirit Sælensminde <k@kirit.com>
2009-06-18 05:44:54 +0000

    Added a fostlib::atexit handler which allows arbitrary lambas to be executed when the process ends. This is now used by the Linux dynlib implementation to ensure that any loaded .so stays loaded until the process ends. This in turn stops segfaults due to database drivers being unloaded whilst exceptions from them are still being handled.

--- Cpp/fost-core/dynlib-linux.cpp
+++ Cpp/fost-core/dynlib-linux.cpp
@@ -7,13 +7,22 @@
 
 
 #include <dlfcn.h>
-#include <iostream>
+#include <fost/atexit.hpp>
+#include <boost/lambda/bind.hpp>
+#include <boost/lambda/construct.hpp>
 
 
 using namespace fostlib;
 
 
 struct fostlib::dynlib::impl {
+    impl()
+    : handle( NULL ) {
+    }
+    ~impl() {
+        if ( handle )
+            dlclose( handle );
+    }
     string name;
     void *handle;
 };
@@ -21,18 +30,18 @@ struct fostlib::dynlib::impl {
 
 fostlib::dynlib::dynlib( const string &lib )
 : m_lib( new impl ) {
+    m_lib->name = lib;
     if ( ( m_lib->handle = dlopen( lib.c_str(), RTLD_NOW ) ) == NULL )
         throw fostlib::exceptions::null( L"dlopen failed for " + lib, string( dlerror() ) );
-    m_lib->name = lib;
-    //std::cout << L"Loaded " << lib << std::endl;
 }
 
 
 fostlib::dynlib::~dynlib() {
-    if ( m_lib ) {
-        if ( m_lib->handle )
-            dlclose( m_lib->handle );
-        //std::cout << L"Unloaded " << m_lib->name << std::endl;
-        delete m_lib;
-    }
+    /*
+        We don't want to actually unload the .so yet as there may well still be objects
+        that it manages.
+        The choice of adding the deletion to atexit here is somewhat arbitrary. It could
+        have just as easily gone in the constructor
+    */
+    fostlib::atexit( boost::lambda::bind( boost::lambda::delete_ptr(), m_lib ) );
 }
