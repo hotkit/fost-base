363b5a882e95f74bacaa72b6caf479155faf1a18
Kirit Sælensminde <k@kirit.com>
2009-06-19 05:33:24 +0000

    The compile script now works a little better.

--- compile.cmd
+++ compile.cmd
@@ -2,11 +2,9 @@
 
 ..\bjam toolset=msvc %*
 
-call ..\boost-version.cmd
 IF NOT ERRORLEVEL 1 (
+    call ..\boost-version.cmd
     for /r %BUILD_DIRECTORY% %%f in (*.pdb) do xcopy /D /Y %%f ..\dist\bin
-)
 
-IF NOT ERRORLEVEL 1 (
     ..\dist\bin\ftest ..\dist\bin\fost-core-test-smoke.dll ..\dist\bin\fost-crypto-test-smoke.dll ..\dist\bin\fost-inet-test-smoke.dll
 )
