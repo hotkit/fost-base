5829970fed2ecb4c659904eb7a7a71c16978fb75
Kirit Sælensminde <k@kirit.com>
2009-07-21 08:26:42 +0000

    Removed the fost-internet tests from fost-base.

--- compile.cmd
+++ compile.cmd
@@ -1,4 +1,5 @@
 @echo off
+echo fost-base
 
 ..\bjam toolset=msvc %*
 
@@ -6,5 +7,5 @@ IF NOT ERRORLEVEL 1 (
     call ..\boost-version.cmd
     for /r %BUILD_DIRECTORY% %%f in (*.pdb) do xcopy /D /Y %%f ..\dist\bin
 
-    ..\dist\bin\ftest ..\dist\bin\fost-core-test-smoke.dll ..\dist\bin\fost-crypto-test-smoke.dll ..\dist\bin\fost-inet-test-smoke.dll
+    ..\dist\bin\ftest ..\dist\bin\fost-core-test-smoke.dll ..\dist\bin\fost-crypto-test-smoke.dll
 )
