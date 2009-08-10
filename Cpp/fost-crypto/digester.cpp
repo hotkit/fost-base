3aa3dbd7849322362441038ff3ab842276c8a101
Kirit Sælensminde <k@kirit.com>
2009-08-10 05:16:38 +0000

    Added null implementation of digester.

---
+++ Cpp/fost-crypto/digester.cpp
@@ -0,0 +1,24 @@
+/*
+    Copyright 2009, Felspar Co Ltd. http://fost.3.felspar.com/
+    Distributed under the Boost Software License, Version 1.0.
+    See accompanying file LICENSE_1_0.txt or copy at
+        http://www.boost.org/LICENSE_1_0.txt
+*/
+
+
+#include "fost-crypto.hpp"
+#include <fost/crypto.hpp>
+#include <fost/exception/not_implemented.hpp>
+
+
+fostlib::digester::digester( string (*)( const string & ) ) {
+    throw fostlib::exceptions::not_implemented( "fostlib::digester::digester( string (*)( const string & ) )" );
+}
+
+fostlib::digester &fostlib::digester::operator << ( const fostlib::string & ) {
+    throw fostlib::exceptions::not_implemented( "fostlib::digester::operator << ( const fostlib::string & )" );
+}
+
+fostlib::digester &fostlib::digester::operator << ( const boost::filesystem::wpath & ) {
+    throw fostlib::exceptions::not_implemented( "fostlib::digester::operator << ( const boost::filesystem::wpath & )" );
+}
