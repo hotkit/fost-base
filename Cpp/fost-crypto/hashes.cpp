42f840d925dccf15a211a9a13e6b36cb11daa5aa
Kirit Sælensminde <k@kirit.com>
2009-08-10 07:28:17 +0000

    The crypto hashing functions now use the new hex_string string type.

--- Cpp/fost-crypto/hashes.cpp
+++ Cpp/fost-crypto/hashes.cpp
@@ -7,6 +7,7 @@
 
 
 #include "fost-crypto.hpp"
+#include <fost/string>
 #include <fost/crypto.hpp>
 #include <openssl/md5.h>
 #include <openssl/sha.h>
@@ -19,25 +20,14 @@ using namespace fostlib;
 namespace {
 
 
-    string hex( const unsigned char *p, size_t l ) {
-        static wchar_t digits[ 16 ] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
-        string ret;
-        for ( size_t i( 0 ); i < l; ++i ) {
-            ret += digits[ ( p[i] >> 4) & 0xF ];
-            ret += digits[ p[i] & 0xF ];
-        }
-        return ret;
-    }
-
-
     template< unsigned char * (*F)(const unsigned char *,std::size_t,unsigned char *), std::size_t L > inline
-    string digest( const string &text ) {
+    hex_string digest( const string &text ) {
         utf8string toproc( coerce< utf8string >( text ) );
         if ( toproc.length() > std::numeric_limits< unsigned long >::max() )
             throw exceptions::out_of_range< size_t >( L"Message is too long to digest", 0, std::numeric_limits< unsigned long >::max(), toproc.length() );
-        boost::scoped_array< unsigned char > result( new unsigned char[ L ] );
-        F( reinterpret_cast< const unsigned char * >( toproc.c_str() ), static_cast< unsigned long>( toproc.length() ), result.get() );
-        return hex( result.get(), L );
+        boost::array< unsigned char, L > result;
+        F( reinterpret_cast< const unsigned char * >( toproc.c_str() ), static_cast< unsigned long>( toproc.length() ), result.data() );
+        return coerce< hex_string >( result );
     }
 
 
@@ -45,11 +35,11 @@ namespace {
 
 
 string fostlib::md5( const string &text ) {
-    return digest< MD5, MD5_DIGEST_LENGTH >( text );
+    return coerce< string >( digest< MD5, MD5_DIGEST_LENGTH >( text ) );
 }
 
 
 string fostlib::sha1( const string &text ) {
-    return digest< SHA1, SHA_DIGEST_LENGTH >( text );
+    return coerce< string >( digest< SHA1, SHA_DIGEST_LENGTH >( text ) );
 }
 
