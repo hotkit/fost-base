ab6a80c28881fecaa9b4bb97e266caf1d8026de0
Kirit Sælensminde <k@kirit.com>
2009-08-10 15:22:19 +0000

    Moved all of the hashing tests into the same place.

--- Cpp/fost-crypto-test/crypto.cpp
+++ Cpp/fost-crypto-test/crypto.cpp
@@ -13,14 +13,6 @@
 FSL_TEST_SUITE( crypto );
 
 
-FSL_TEST_FUNCTION( sha1 ) {
-    // http://en.wikipedia.org/wiki/Examples_of_SHA_digests
-    FSL_CHECK_EQ( fostlib::sha1( fostlib::string() ), L"da39a3ee5e6b4b0d3255bfef95601890afd80709" );
-    FSL_CHECK_EQ( fostlib::sha1( L"The quick brown fox jumps over the lazy dog" ), L"2fd4e1c67a2d28fced849ee1bb76e7391b93eb12" );
-    FSL_CHECK_EQ( fostlib::sha1( L"The quick brown fox jumps over the lazy cog" ), L"de9f2c7fd25e1b3afad3e85a0bd17d9b100db4b3" );
-}
-
-
 FSL_TEST_FUNCTION( sha1_hmac ) {
     // http://docs.amazonwebservices.com/AmazonS3/2006-03-01/RESTAuthentication.html
     FSL_CHECK_EQ( fostlib::sha1_hmac(
