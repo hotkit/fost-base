6e0c4a0d50bf634149126ad3762674d56a745425
Kirit Sælensminde <k@kirit.com>
2009-08-20 09:25:30 +0000

    Tests are now run as part of the build and the PDB files are managed at the end of the build process.

--- compile.cmd
+++ compile.cmd
@@ -2,10 +2,3 @@
 echo fost-base
 
 ..\bjam toolset=msvc %*
-
-IF NOT ERRORLEVEL 1 (
-    call ..\boost-version.cmd
-    for /r %BUILD_DIRECTORY% %%f in (*.pdb) do xcopy /D /Y %%f ..\dist\bin
-
-    ..\dist\bin\ftest ..\dist\bin\fost-core-test-smoke.dll ..\dist\bin\fost-crypto-test-smoke.dll
-)
