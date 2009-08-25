896efaf3eef5c3633eda1a060002d15b6e81187d
Kirit Sælensminde <k@kirit.com>
2009-08-25 13:04:33 +0000

    We can now generated detailed error messages for Windows errors. Failing to load a DLL now does this.

--- Cpp/fost-core/string-utilities-win.cpp
+++ Cpp/fost-core/string-utilities-win.cpp
@@ -25,3 +25,20 @@ fostlib::string fostlib::guid() {
     }
 }
 
+
+fostlib::string fostlib::format_last_error() {
+    LPWSTR buffer = NULL;
+    DWORD result = FormatMessage(
+        FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_ALLOCATE_BUFFER,
+        0, GetLastError(), 0,
+        reinterpret_cast< LPWSTR >(&buffer), 0,
+        NULL
+    );
+    fostlib::string error;
+    if ( result )
+        error = fostlib::string(buffer, buffer + result);
+    else
+        error = L"No detailed error message is available";
+    LocalFree(buffer);
+    return error;
+}
