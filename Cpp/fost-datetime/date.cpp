7da2cc24335622a99524d14131e60252d6ed1f12
Kirit Sælensminde <k@kirit.com>
2009-09-01 04:54:10 +0000

    Implemented some more basic features of fostlib::date.

--- Cpp/fost-datetime/date.cpp
+++ Cpp/fost-datetime/date.cpp
@@ -9,11 +9,19 @@
 #include "fost-datetime.hpp"
 #include <fost/detail/date.hpp>
 
+#include <boost/date_time/gregorian/gregorian.hpp>
+
 
 using namespace fostlib;
 
 
 fostlib::date::date() {
+}
+fostlib::date::date( boost::gregorian::date d )
+: m_date( d ) {
+}
+fostlib::date::date( int year, int month, int day )
+: m_date( year, month, day ) {
 }
 
 
@@ -22,4 +30,16 @@ bool fostlib::date::operator == ( const date &r ) const {
 }
 bool fostlib::date::operator != ( const date &r ) const {
     return m_date != r.m_date;
+}
+
+
+string fostlib::coercer< string, date >::coerce( date d ) {
+    return string( boost::gregorian::to_iso_extended_string(fostlib::coerce< boost::gregorian::date >( d )) );
+}
+
+date fostlib::coercer< date, json >::coerce( const json &j ) {
+    nullable< string > s( j.get< string >() );
+    if ( s.isnull() )
+        throw exceptions::not_implemented("fostlib::coercer< date, json >::coerce( const json &j ) -- with non-string JSON");
+    return date( boost::gregorian::from_string( fostlib::coerce< std::string >( s.value() ) ) );
 }
--- Cpp/include/fost/detail/date.hpp
+++ Cpp/include/fost/detail/date.hpp
@@ -11,7 +11,7 @@
 #pragma once
 
 
-#include <fost/config.hpp>
+#include <fost/core>
 #include <boost/date_time/gregorian/gregorian_types.hpp>
 
 
@@ -20,12 +20,54 @@ namespace fostlib {
 
     class FOST_DATETIME_DECLSPEC date {
         boost::gregorian::date m_date;
+        friend struct fostlib::coercer< boost::gregorian::date, date >;
     public:
         date();
+        explicit date( boost::gregorian::date );
+        date( int year, int month, int day );
 
         bool operator == ( const date &d ) const;
         bool operator != ( const date &d ) const;
     };
+
+
+    template<>
+    struct coercer< boost::gregorian::date, date > {
+        boost::gregorian::date coerce( date d ) {
+            return d.m_date;
+        }
+    };
+    template<>
+    struct FOST_DATETIME_DECLSPEC coercer< date, boost::gregorian::date > {
+        date coerce( boost::gregorian::date );
+    };
+
+    template<>
+    struct FOST_DATETIME_DECLSPEC coercer< string, date > {
+        string coerce( date );
+    };
+
+    template<>
+    struct coercer< json, date > {
+        json coerce( date d ) {
+            return json( fostlib::coerce< string >( d ) );
+        }
+    };
+    template<>
+    struct FOST_DATETIME_DECLSPEC coercer< date, json > {
+        date coerce( const json & );
+    };
+
+
+}
+
+
+namespace std {
+
+
+    inline fostlib::ostream &operator << ( fostlib::ostream &o, fostlib::date d ) {
+        return o << fostlib::coerce< fostlib::string >( d );
+    }
 
 
 }
