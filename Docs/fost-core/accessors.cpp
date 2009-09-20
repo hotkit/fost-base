b5ad62839233b862f4d4943416dbad6c942ba36c
Kirit Sælensminde <k@kirit.com>
2009-09-20 18:04:20 +0000

    Added some documentation for the accessors class.

---
+++ Docs/fost-core/accessors.cpp
@@ -0,0 +1,16 @@
+/** \defgroup fost_core_accessor accessor
+    \ingroup fost_core
+
+    \brief A mechanism for making public attributes look like accessor methods.
+
+*/
+
+/** \class fostlib::accessor fost/accessors.hpp fost/core
+    \ingroup fost_core_accessor
+
+    \brief A class which makes attributes syntactically behave like accessor methods.
+
+    \sa
+    - <a href="http://www.kirit.com/C%2B%2B%20killed%20the%20get%20%26%20set%20accessors">C++ killed the get & set accessors</a>
+    - <a href="http://www.kirit.com/C%2B%2B%20killed%20the%20get%20%26%20set%20accessors/A%20simple%20meta-accessor">A simple meta-accessor</a>
+*/
