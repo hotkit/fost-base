7ba412b15ece3a8d832d7d9eb313adbd155b81e7
Kirit Sælensminde <k@kirit.com>
2009-09-20 11:25:49 +0000

    Improved the settings database documentation.

--- Docs/fost-core/index.hpp
+++ Docs/fost-core/index.hpp
@@ -7,9 +7,9 @@
 
     - \ref fost_core_string -- A Unicode string class modelled on the `std::string` interface, but using the native character size internally (char on Linux, wchar_t on Windows), but presenting a consistent UTF-32 based exterior. This allows Unicode strings to be used efficiently with portable source code between Windows and Linux.
     - \ref fost_core_settings -- A settings database used for software configuration.
-    - [[JSON]] -- A JSON parser and printer together with suitable data types to handle JSON objects, arrays and atoms.
+    - \ref fost_core_json -- A JSON parser and printer together with suitable data types to handle JSON objects, arrays and atoms.
     - \ref fost_core_coerce -- A type safe type coercion library which is extensible for user defined types.
-    - [[thread]] -- A threading library providing futures and worker threads with thread affinity.
+    - \ref fost_core_threading -- A threading library providing futures and worker threads with thread affinity.
 
     There are also a number of smaller support libraries.
 */
