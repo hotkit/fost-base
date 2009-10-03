55a16454cc2ba30456ad4e8416a09fb28d4d965c
Kirit Sælensminde <k@kirit.com>
2009-10-03 07:42:40 +0000

    New structure for documentation now that fost-meta is used to pull it all together.

---
+++ Docs/modules.hpp
@@ -0,0 +1,4 @@
+/** \defgroup fost_base fost-base
+
+    \brief This package contains all of the basic Fost 4 functionality for building DLLs/shared-objects, for command line applications and for unit testing.
+*/
