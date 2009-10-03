6e8fe7b603bc24d85f8e3422eed59aeaccc52f3d
Kirit Sælensminde <k@kirit.com>
2009-10-03 09:34:54 +0000

    Added brief descriptions for the remaining modules.

--- Docs/modules.hpp
+++ Docs/modules.hpp
@@ -2,3 +2,31 @@
 
     \brief This package contains all of the basic Fost 4 functionality for building DLLs/shared-objects, for command line applications and for unit testing.
 */
+
+
+/** \defgroup fost_cli fost-cli
+    \ingroup fost_base
+
+    \brief Contains functionality for command line interface programs.
+*/
+
+
+/** \defgroup fost_crypto fost-crypto
+    \ingroup fost_base
+
+    \brief Various cryptographic building blocks.
+*/
+
+
+/** \defgroup fost_datetime fost-datetime
+    \ingroup fost_base
+
+    \brief Date and time handling functionality.
+*/
+
+
+/** \defgroup fost_test fost-test
+    \ingroup fost_base
+
+    \brief Testing library.
+*/
