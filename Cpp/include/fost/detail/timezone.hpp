cd46a0dd7ad1753f99028863ef34d3e8d881083e
Kirit Sælensminde <k@kirit.com>
2009-10-27 15:08:17 +0000

    Added a sketch for how the basic time zone information will look in the APIs.

---
+++ Cpp/include/fost/detail/timezone.hpp
@@ -0,0 +1,43 @@
+/*
+    Copyright 2009, Felspar Co Ltd. http://fost.3.felspar.com/
+    Distributed under the Boost Software License, Version 1.0.
+    See accompanying file LICENSE_1_0.txt or copy at
+        http://www.boost.org/LICENSE_1_0.txt
+*/
+
+
+#ifndef FOST_DETAIL_TIMEZONE_HPP
+#define FOST_DETAIL_TIMEZONE_HPP
+#pragma once
+
+
+#include <fost/core>
+
+
+namespace fostlib {
+
+
+    class FOST_DATETIME_DECLSPEC zoneinfo {
+        public:
+            zoneinfo();
+
+            accessors< string > name;
+    };
+
+
+}
+
+
+namespace std {
+
+
+    inline fostlib::ostream &operator << ( fostlib::ostream &o, fostlib::zoneinfo t ) {
+        return o << t.name();
+    }
+
+
+}
+
+
+#endif // FOST_DETAIL_TIMEZONE_HPP
+
