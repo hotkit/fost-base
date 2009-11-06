7b3582cffec9adc097cdb6c2cfce72ff5177535b
Kirit Sælensminde <k@kirit.com>
2009-11-06 06:35:32 +0000

    Removed the rest of the deprecated type utf8string.

--- Cpp/include/fost/coerce/win.hpp
+++ Cpp/include/fost/coerce/win.hpp
@@ -1,5 +1,5 @@
 /*
-    Copyright 2008, Felspar Co Ltd. http://fost.3.felspar.com/
+    Copyright 2008-2009, Felspar Co Ltd. http://fost.3.felspar.com/
     Distributed under the Boost Software License, Version 1.0.
     See accompanying file LICENSE_1_0.txt or copy at
         http://www.boost.org/LICENSE_1_0.txt
@@ -79,8 +79,8 @@ namespace fostlib {
     };
 
     template<>
-    struct FOST_CORE_DECLSPEC coercer< utf8string, _variant_t > {
-        utf8string coerce( const _variant_t &v );
+    struct FOST_CORE_DECLSPEC coercer< utf8_string, _variant_t > {
+        utf8_string coerce( const _variant_t &v );
     };
 
     template<>
