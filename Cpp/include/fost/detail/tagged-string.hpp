d106940aa2de538276a8c9df86edb7d5474ed47d
Kirit Sælensminde <k@kirit.com>
2009-11-08 03:40:01 +0000

    Moved the ASCII string into its own header.

--- Cpp/include/fost/detail/tagged-string.hpp
+++ Cpp/include/fost/detail/tagged-string.hpp
@@ -151,44 +151,6 @@ namespace fostlib {
     };
 
 
-    struct FOST_CORE_DECLSPEC ascii_string_tag {
-        static void do_encode( fostlib::nliteral from, std::string &into );
-        static void do_encode( const std::string &from, std::string &into );
-        static void check_encoded( const std::string &s );
-    };
-    typedef tagged_string< ascii_string_tag, std::string > ascii_string;
-
-    template<>
-    struct FOST_CORE_DECLSPEC coercer< ascii_string, std::vector< ascii_string::value_type > > {
-        ascii_string coerce( const std::vector< ascii_string::value_type > &v );
-    };
-
-    template<>
-    struct FOST_CORE_DECLSPEC coercer< string, ascii_string > {
-        string coerce( const ascii_string &s );
-    };
-    template<>
-    struct FOST_CORE_DECLSPEC coercer< ascii_string, string > {
-        ascii_string coerce( const string &s );
-    };
-    template<>
-    struct coercer< std::string, ascii_string > {
-        const std::string &coerce( const ascii_string &s ) {
-            return s.underlying();
-        }
-    };
-    template<>
-    struct coercer< ascii_string, std::string > {
-        ascii_string coerce( const std::string &s ) {
-            return ascii_string( s );
-        }
-    };
-    template<>
-    struct FOST_CORE_DECLSPEC coercer< std::wstring, ascii_string > {
-        std::wstring coerce( const ascii_string &s );
-    };
-
-
 }
 
 
@@ -215,6 +177,9 @@ namespace std {
 
 
 }
+
+
+#include <fost/detail/ascii.hpp>
 
 
 #endif // FOST_TAGGED_STRING_HPP
