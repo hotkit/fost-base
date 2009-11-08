90c6ee7a4a49da842aa09bd161f43b05a4fef47e
Kirit Sælensminde <k@kirit.com>
2009-11-08 06:42:30 +0000

    Added a printable ASCII string type.

--- Cpp/fost-core/string-tagged.cpp
+++ Cpp/fost-core/string-tagged.cpp
@@ -34,54 +34,6 @@ void fostlib::utf8_string_tag::check_encoded( const std::string &s ) {
 
 
 /*
-    fostlib::ascii_string
-*/
-
-void fostlib::ascii_string_tag::do_encode( fostlib::nliteral from, std::string &into ) {
-    throw fostlib::exceptions::not_implemented( L"fostlib::ascii_string_tag::do_encode( fostlib::nliteral from, std::string &into )" );
-}
-void fostlib::ascii_string_tag::do_encode( const std::string &from, std::string &into ) {
-    throw fostlib::exceptions::not_implemented( L"fostlib::ascii_string_tag::do_encode( const std::string &from, std::string &into )" );
-}
-void fostlib::ascii_string_tag::check_encoded( const std::string &s ) {
-    std::size_t p = 0;
-    try {
-        for ( std::string::const_iterator c( s.begin() ); c != s.end(); ++c, ++p )
-            if ( *c < 1 || *c > 127 )
-                throw fostlib::exceptions::out_of_range< int >( L"ASCII characters outside valid range", 1, 127, *c );
-    } catch ( fostlib::exceptions::exception &e ) {
-        e.info() << L"String up until this point: "
-            << fostlib::coerce< fostlib::string >( fostlib::coerce< fostlib::ascii_string >( s.substr( 0, p ) ) )
-            << L"\nChecked " << p << " characters out of " << s.size() << std::endl;
-        throw;
-    }
-}
-
-fostlib::ascii_string fostlib::coercer<
-    fostlib::ascii_string, std::vector< fostlib::ascii_string::value_type >
->::coerce( const std::vector< fostlib::ascii_string::value_type > &v ) {
-    return ascii_string( std::string( &v[0], v.size() ) );
-}
-fostlib::ascii_string fostlib::coercer< fostlib::ascii_string, fostlib::string >::coerce( const fostlib::string &s ) {
-    fostlib::utf8_string utf8 = fostlib::coerce< fostlib::utf8_string >( s );
-    return fostlib::ascii_string( utf8.underlying() );
-}
-
-fostlib::string fostlib::coercer< fostlib::string, fostlib::ascii_string >::coerce( const fostlib::ascii_string &s ) {
-    return string(s.underlying().c_str());
-}
-
-std::wstring fostlib::coercer<
-    std::wstring, fostlib::ascii_string
->::coerce( const fostlib::ascii_string &s ) {
-    std::wstring r; r.resize(s.underlying().length());
-    for ( std::size_t p = 0; p < s.underlying().length(); ++p )
-        r[p] = s.underlying()[p];
-    return r;
-}
-
-
-/*
     fostlib::base64_string
 */
 
