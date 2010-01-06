ec6e1a04d2a98d7fa4e41ed29a8939088147c0d2
Kirit Sælensminde <k@kirit.com>
2010-01-06 14:32:58 +0000

    We should only trigger a debug session for Windows.

--- Cpp/fost-core/exception.cpp
+++ Cpp/fost-core/exception.cpp
@@ -1,5 +1,5 @@
 /*
-    Copyright 2001-2009, Felspar Co Ltd. http://fost.3.felspar.com/
+    Copyright 2001-2010, Felspar Co Ltd. http://fost.3.felspar.com/
     Distributed under the Boost Software License, Version 1.0.
     See accompanying file LICENSE_1_0.txt or copy at
         http://www.boost.org/LICENSE_1_0.txt
@@ -21,7 +21,7 @@ namespace {
 void fostlib::absorbException() throw () {
     // An exception is in the process of being thrown away.
     // We want to be very careful not to do anything that may throw again.
-#ifdef _DEBUG
+#if defined(FOST_OS_WINDOWS) && defined(_DEBUG)
     // This will allow all debug builds to attach to a debugger to offending run.
     // It's unclear what this may do with something like IIS...
     __asm int 3
