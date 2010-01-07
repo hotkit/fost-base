868071fbd13947869340a8a2c9ce897baad67ac4
Kirit Sælensminde <k@kirit.com>
2010-01-07 13:40:44 +0000

    Fixed up some tests so that the expected answers are also unsigned when the tests produce unsigned output.

--- Cpp/fost-core/char-encoding-tests.cpp
+++ Cpp/fost-core/char-encoding-tests.cpp
@@ -1,5 +1,5 @@
 /*
-    Copyright 2009, Felspar Co Ltd. http://fost.3.felspar.com/
+    Copyright 2009-2010, Felspar Co Ltd. http://fost.3.felspar.com/
     Distributed under the Boost Software License, Version 1.0.
     See accompanying file LICENSE_1_0.txt or copy at
         http://www.boost.org/LICENSE_1_0.txt
@@ -19,26 +19,26 @@ FSL_TEST_SUITE( char_encoding );
 
 
 FSL_TEST_FUNCTION( lengths_narrow ) {
-    FSL_CHECK_EQ( length( "" ), 0 );
-    FSL_CHECK_EQ( length( "a" ), 1 );
+    FSL_CHECK_EQ( length( "" ), 0u );
+    FSL_CHECK_EQ( length( "a" ), 1u );
 }
 
 FSL_TEST_FUNCTION( lengths_utf8 ) {
-    FSL_CHECK_EQ( utf8length( 0x20 ), 1 ); // space
-    FSL_CHECK_EQ( utf8length( 0xe5 ), 2 ); // latin small letter a with ring above
-    FSL_CHECK_EQ( utf8length( 0x3b3 ), 2 ); // greek small letter gamma
-    FSL_CHECK_EQ( utf8length( 0x2014 ), 3 ); // em dash
-    FSL_CHECK_EQ( utf8length( 0xffe1 ), 3 ); // full width pound sign
-    FSL_CHECK_EQ( utf8length( 0x1d11e ), 4 ); // treble clef
+    FSL_CHECK_EQ( utf8length( 0x20 ), 1u ); // space
+    FSL_CHECK_EQ( utf8length( 0xe5 ), 2u ); // latin small letter a with ring above
+    FSL_CHECK_EQ( utf8length( 0x3b3 ), 2u ); // greek small letter gamma
+    FSL_CHECK_EQ( utf8length( 0x2014 ), 3u ); // em dash
+    FSL_CHECK_EQ( utf8length( 0xffe1 ), 3u ); // full width pound sign
+    FSL_CHECK_EQ( utf8length( 0x1d11e ), 4u ); // treble clef
 }
 
 FSL_TEST_FUNCTION( lengths_utf16 ) {
-    FSL_CHECK_EQ( utf16length( 0x20 ), 1 ); // space
-    FSL_CHECK_EQ( utf16length( 0xe5 ), 1 ); // latin small letter a with ring above
-    FSL_CHECK_EQ( utf16length( 0x3b3 ), 1 ); // greek small letter gamma
-    FSL_CHECK_EQ( utf16length( 0x2014 ), 1 ); // em dash
-    FSL_CHECK_EQ( utf16length( 0xffe1 ), 1 ); // full width pound sign
-    FSL_CHECK_EQ( utf16length( 0x1d11e ), 2 ); // treble clef
+    FSL_CHECK_EQ( utf16length( 0x20 ), 1u ); // space
+    FSL_CHECK_EQ( utf16length( 0xe5 ), 1u ); // latin small letter a with ring above
+    FSL_CHECK_EQ( utf16length( 0x3b3 ), 1u ); // greek small letter gamma
+    FSL_CHECK_EQ( utf16length( 0x2014 ), 1u ); // em dash
+    FSL_CHECK_EQ( utf16length( 0xffe1 ), 1u ); // full width pound sign
+    FSL_CHECK_EQ( utf16length( 0x1d11e ), 2u ); // treble clef
 }
 
 
--- Cpp/fost-core/json-object-basic-tests.cpp
+++ Cpp/fost-core/json-object-basic-tests.cpp
@@ -1,5 +1,5 @@
 /*
-    Copyright 2008, Felspar Co Ltd. http://fost.3.felspar.com/
+    Copyright 2008-2010, Felspar Co Ltd. http://fost.3.felspar.com/
     Distributed under the Boost Software License, Version 1.0.
     See accompanying file LICENSE_1_0.txt or copy at
         http://www.boost.org/LICENSE_1_0.txt
@@ -35,11 +35,11 @@ FSL_TEST_FUNCTION( fetching ) {
 FSL_TEST_FUNCTION( insert ) {
     fostlib::json a = fostlib::json::object_t();
     fostlib::jcursor( L"key" ).insert( a, fostlib::json( L"value" ) );
-    FSL_CHECK_EQ( a.size(), 1 );
+    FSL_CHECK_EQ( a.size(), 1u );
     FSL_CHECK_EQ( a[ L"key" ], fostlib::json( L"value" ) );
     fostlib::jcursor( L"key 2" ).insert( a, fostlib::json( 10 ) );
     FSL_CHECK_EXCEPTION( fostlib::jcursor( L"key 2" ).insert( a, fostlib::json( 12 ) ), fostlib::exceptions::not_null& );
-    FSL_CHECK_EQ( a.size(), 2 );
+    FSL_CHECK_EQ( a.size(), 2u );
     FSL_CHECK_EQ( a[ L"key 2" ], fostlib::json( 10 ) );
 }
 
@@ -47,11 +47,11 @@ FSL_TEST_FUNCTION( insert ) {
 FSL_TEST_FUNCTION( set ) {
     fostlib::json a = fostlib::json::object_t();
     FSL_CHECK_NOTHROW( fostlib::jcursor( L"key" )( a ) = fostlib::json( L"value" ) );
-    FSL_CHECK_EQ( a.size(), 1 );
+    FSL_CHECK_EQ( a.size(), 1u );
     FSL_CHECK_EQ( a[ L"key" ], fostlib::json( L"value" ) );
     FSL_CHECK_NOTHROW( fostlib::jcursor( L"key 2" )( a ) = fostlib::json( 10 ) );
     FSL_CHECK_NOTHROW( fostlib::jcursor( L"key 2" )( a ) = fostlib::json( 12 ) );
-    FSL_CHECK_EQ( a.size(), 2 );
+    FSL_CHECK_EQ( a.size(), 2u );
     FSL_CHECK_EQ( a[ L"key 2" ], fostlib::json( 12 ) );
 }
 
@@ -59,12 +59,12 @@ FSL_TEST_FUNCTION( set ) {
 FSL_TEST_FUNCTION( replace ) {
     fostlib::json a = fostlib::json::object_t();
     fostlib::jcursor( L"key" ).insert( a, L"value" );
-    FSL_CHECK_EQ( a.size(), 1 );
+    FSL_CHECK_EQ( a.size(), 1u );
     FSL_CHECK_EQ( a[ L"key" ], fostlib::json( L"value" ) );
     FSL_CHECK_EXCEPTION( fostlib::jcursor( L"key 2" ).replace( a, 10 ), fostlib::exceptions::null& );
     fostlib::jcursor( L"key 2" ).insert( a, 10 );
     fostlib::jcursor( L"key 2" )( a ) = fostlib::json( 12 );
-    FSL_CHECK_EQ( a.size(), 2 );
+    FSL_CHECK_EQ( a.size(), 2u );
     FSL_CHECK_EQ( a[ L"key 2" ], fostlib::json( 12 ) );
 }
 
