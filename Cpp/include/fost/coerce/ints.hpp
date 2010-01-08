05ed9be5c2061600b6354ebf39b127b7e4e238a7
Kirit Sælensminde <k@kirit.com>
2010-01-08 14:36:26 +0000

    Changed the layout to make it more friendly to narrower editors.

--- Cpp/include/fost/coerce/ints.hpp
+++ Cpp/include/fost/coerce/ints.hpp
@@ -19,16 +19,29 @@ namespace fostlib {
 
 
     template< typename I >
-    struct coercer< int64_t, I, typename boost::enable_if< boost::is_integral< I > >::type > {
+    struct coercer<
+        int64_t, I,
+        typename boost::enable_if< boost::is_integral< I > >::type
+    > {
         int64_t coerce( I i ) {
             return int64_t( i );
         }
     };
     template< typename I >
-    struct coercer< I, int64_t, typename boost::enable_if< boost::is_integral< I > >::type > {
+    struct coercer<
+        I, int64_t,
+        typename boost::enable_if< boost::is_integral< I > >::type
+    > {
         I coerce( int64_t i ) {
-            if ( i > std::numeric_limits< I >::max() || i < std::numeric_limits< I >::min() )
-                throw fostlib::exceptions::out_of_range< int64_t >( std::numeric_limits< I >::min(), std::numeric_limits< I >::max(), i );
+            if (
+                i > std::numeric_limits< I >::max()
+                || i < std::numeric_limits< I >::min()
+            )
+                throw fostlib::exceptions::out_of_range< int64_t >(
+                    std::numeric_limits< I >::min(),
+                    std::numeric_limits< I >::max(),
+                    i
+                );
             return I( i );
         }
     };
