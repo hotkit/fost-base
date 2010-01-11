827bf7678c4c2ac82934a98d9351f8d818cde529
Kirit Sælensminde <k@kirit.com>
2010-01-11 14:31:35 +0000

    The argument parser no longer accepts forward slashes for command line switches. This fixes a problem where absolute paths cannot be used on Unix machines because they will always start with a forward slash.
    * fost-cli now uses the autotest rule.
    * Added a couple of tests for the argument counting.
    * Added some brief Doxygen documentation to some of the arguments methods.

--- Cpp/include/fost/main.hpp
+++ Cpp/include/fost/main.hpp
@@ -1,5 +1,5 @@
 /*
-    Copyright 2008-2009, Felspar Co Ltd. http://fost.3.felspar.com/
+    Copyright 2008-2010, Felspar Co Ltd. http://fost.3.felspar.com/
     Distributed under the Boost Software License, Version 1.0.
     See accompanying file LICENSE_1_0.txt or copy at
         http://www.boost.org/LICENSE_1_0.txt
@@ -40,13 +40,17 @@ namespace fostlib {
     FOST_CLI_DECLSPEC int main_exec(
         const ini_settings &settings,
         ostream &out,
-        arguments &args, int (*main_f)( fostlib::ostream &, fostlib::arguments & )
+        arguments &args,
+        int (*main_f)( fostlib::ostream &, fostlib::arguments & )
     );
     // Used by FSL_MAIN_INTERPRETER
     FOST_CLI_DECLSPEC int main_exec(
         const ini_settings &settings,
         ostream &out,
-        arguments &args, int (*main_f)( const ini_settings &, fostlib::ostream &, fostlib::arguments & )
+        arguments &args,
+        int (*main_f)(
+            const ini_settings &, fostlib::ostream &, fostlib::arguments &
+        )
     );
 
 
@@ -54,18 +58,25 @@ namespace fostlib {
 
 
 #define FSL_MAIN_PRIVATE_COMMON( exe_name, banner_text ) \
-        const fostlib::ini_settings config_settings( fostlib::string( exe_name ), fostlib::string( banner_text ) );
+        const fostlib::ini_settings config_settings( \
+            fostlib::string( exe_name ), fostlib::string( banner_text ) \
+        );
 
 #define FSL_MAIN_PRIVATE_SIMPLEMAIN() \
         int main_body( fostlib::ostream &, fostlib::arguments & );
 #define FSL_MAIN_PRIVATE_INTERPRETERMAIN() \
-        int main_body( const fostlib::ini_settings &, fostlib::ostream &, fostlib::arguments & );
+        int main_body( \
+            const fostlib::ini_settings &, fostlib::ostream &, fostlib::arguments & \
+        );
 
-#ifdef WIN32
+#ifdef FOST_OS_WINDOWS
 
     #define FSL_MAIN_PRIVATE_CALLER( exe_name, banner_text ) \
         int __cdecl wmain( int argc, wchar_t *argv[ ], wchar_t *envp[ ] ) { \
-            fostlib::com_hr( ::CoInitializeEx( NULL, COINIT_APARTMENTTHREADED ), L"CoInitializeEx at program start" ); \
+            fostlib::com_hr( \
+                ::CoInitializeEx( NULL, COINIT_APARTMENTTHREADED ), \
+                L"CoInitializeEx at program start" \
+            ); \
             fostlib::arguments args( argc, argv, envp ); \
             return fostlib::main_exec( config_settings, std::wcout, args, main_body ); \
         } \
@@ -82,24 +93,29 @@ namespace fostlib {
 
 #endif
 
-/*
+/**  \brief A muliti-platform Unicode aware main that handles argument parsing
+
     This version of main is suitable for most programs that simply need to take settings
     and start some task for the user.
-    This version will load the INI file for the program and the -i command line switch is handled.
+
+    This version will load the INI file for the program and the -i command line switch
+    is handled.
 */
 #define FSL_MAIN( exe_name, banner_text ) \
         FSL_MAIN_PRIVATE_COMMON( exe_name, banner_text ) \
         FSL_MAIN_PRIVATE_SIMPLEMAIN() \
         FSL_MAIN_PRIVATE_CALLER( exe_name, banner_text )
 
-/*
-    This version of main is suitable for programs that act as interpreters for other languages.
-    The standard_arguments must be called explicitly, but the interpreter has the opportunity
-    to layer in any extra configuration settings that it may want to do before the standard
-    argument handling is done. The version of main written for this will also be passed the
-    settings object.
-    NB: The INI file for the program is not automatically loaded by this version. The -i command
-    line switch is not handled either.
+/** \brief A multi-platform Unicode aware main for programming language interpreters
+
+    This version of main is suitable for programs that act as interpreters for
+    other languages. The standard_arguments must be called explicitly, but the
+    interpreter has the opportunity to layer in any extra configuration settings
+    that it may want to do before the standard argument handling is done. The
+    version of main written for this will also be passed the settings object.
+
+    NB: The INI file for the program is not automatically loaded by this version.
+    The -i command line switch is not handled either.
 */
 #define FSL_MAIN_INTERPRETER( exe_name, banner_text ) \
         FSL_MAIN_PRIVATE_COMMON( exe_name, banner_text ) \
