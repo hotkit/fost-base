bcad91e0b0a68eb250f8dd68d014e8aa9a7c3e1f
Kirit Sælensminde <k@kirit.com>
2010-01-30 14:59:36 +0000

    Started to put together the work pool implementation.

--- fost-base/Cpp/fost-core/pool.cpp
+++ fost-base/Cpp/fost-core/pool.cpp
@@ -10,47 +10,88 @@
 #include <fost/workerpool.hpp>
 
 
-using namespace fostlib;
+namespace {
+    struct worker {
+        static fostlib::counter worker_count;
 
+        worker() {
+            ++worker_count;
+        }
 
-struct fostlib::workerpool::implementation {
-    boost::mutex m_mutex;
-    std::vector< boost::shared_ptr< worker > > m_available;
-    std::size_t m_peak;
-};
+        typedef fostlib::in_process< worker > worker_type;
+        typedef std::deque< boost::shared_ptr< worker_type > > workers_type;
+        typedef std::deque< fostlib::workerpool< void >::function_type > queue_type;
 
+        bool exec(workers_type::value_type) const;
 
-fostlib::workerpool::workerpool()
-: impl(new implementation) {
-    impl->m_peak = 0;
+        static boost::mutex &g_mutex() {
+            static boost::mutex m;
+            return m;
+        }
+        static worker::workers_type &g_workers() {
+            static workers_type w;
+            return w;
+        }
+        static queue_type &g_queue() {
+            static queue_type q;
+            return q;
+        }
+
+        static worker *make_worker() {
+            return new worker;
+        }
+        static void execute( workers_type::value_type w ) {
+            w->asynchronous< bool >(
+                boost::lambda::bind( &::worker::exec, boost::lambda::_1, w )
+            );
+        }
+    };
 }
-fostlib::workerpool::~workerpool() {
-    delete impl;
+
+
+fostlib::counter worker::worker_count;
+
+
+fostlib::workerpool< void >::~workerpool() {
 }
 
 
-boost::shared_ptr< worker > fostlib::workerpool::assign() {
-    boost::mutex::scoped_lock lock(impl->m_mutex);
-    if ( !impl->m_available.size() )
-        return boost::shared_ptr< worker >( new worker );
-    else {
-        boost::shared_ptr< worker > w = impl->m_available.back();
-        impl->m_available.pop_back();
-        return w;
+bool ::worker::exec( workers_type::value_type self ) const {
+    fostlib::workerpool< void >::function_type f;
+    { // Grab the head of the work queue to work on
+        boost::mutex::scoped_lock lock(g_mutex());
+        if ( g_queue().empty() ) {
+            // Nothing to do, just add myself back to the queue and exit
+            return false;
+        } else {
+            f = g_queue().front();
+            g_queue().pop_front();
+        }
+    }
+    // Now that we've dropped the lock we can do the work
+    return true;
+}
+
+void fostlib::workerpool< void >::operator () ( function_type f ) {
+    boost::mutex::scoped_lock lock(::worker::g_mutex());
+    // Put the work onto the queue
+    ::worker::g_queue().push_back( f );
+    // If there is a worker available (or there aren't enough worker to fill the pool yet)
+    if ( ::worker::g_workers().empty() ) {
+        // If there isn't a worker available maybe we can create one
+        if ( ::worker::worker_count.value() < 2 ) {
+            ::worker::workers_type::value_type w( new ::worker::worker_type(
+                ::worker::make_worker
+            ) );
+            ::worker::execute(w);
+        }
+    } else {
+        // Get this worker to grab an item from the queue
+        ::worker::workers_type::value_type w = ::worker::g_workers().front();
+        ::worker::g_workers().pop_front();
     }
 }
-void fostlib::workerpool::replace( boost::shared_ptr< worker > w ) {
-    boost::mutex::scoped_lock lock(impl->m_mutex);
-    impl->m_available.push_back(w);
-    impl->m_peak = std::max( impl->m_peak, impl->m_available.size() );
-}
 
-
-std::size_t fostlib::workerpool::available() {
-    boost::mutex::scoped_lock lock(impl->m_mutex);
-    return impl->m_available.size();
-}
-std::size_t fostlib::workerpool::peak_used() {
-    boost::mutex::scoped_lock lock(impl->m_mutex);
-    return impl->m_peak;
+void fostlib::workerpool< void >::operator () () {
+    throw exceptions::not_implemented("workerpool<void>::operator () ()");
 }
