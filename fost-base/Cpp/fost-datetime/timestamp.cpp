565d8e9e461590ade078cd6a114a7f44a57ed090
Kirit Sælensminde <k@kirit.com>
2010-02-27 14:33:21 +0000

    Moved the final header in the date/time library to a proper location.

--- fost-base/Cpp/fost-datetime/timestamp.cpp
+++ fost-base/Cpp/fost-datetime/timestamp.cpp
@@ -1,5 +1,5 @@
 /*
-    Copyright 2000-2009, Felspar Co Ltd. http://fost.3.felspar.com/
+    Copyright 2000-2010, Felspar Co Ltd. http://fost.3.felspar.com/
     Distributed under the Boost Software License, Version 1.0.
     See accompanying file LICENSE_1_0.txt or copy at
         http://www.boost.org/LICENSE_1_0.txt
@@ -7,10 +7,8 @@
 
 
 #include "fost-datetime.hpp"
-#include <fost/detail/timestamp.hpp>
+#include <fost/timestamp.hpp>
 #include <boost/date_time/posix_time/posix_time.hpp>
-
-#include <fost/exception/not_implemented.hpp>
 
 
 using namespace fostlib;
@@ -42,10 +40,15 @@ string fostlib::coercer< string, timestamp >::coerce( timestamp t ) {
     s[10] = ' ';
     return fostlib::coerce< string >(fostlib::coerce< ascii_string >(s));
 }
-rfc1123_timestamp fostlib::coercer< rfc1123_timestamp, timestamp >::coerce( timestamp t ) {
+rfc1123_timestamp fostlib::coercer<
+    rfc1123_timestamp, timestamp
+>::coerce( timestamp t ) {
     boost::posix_time::ptime ts = fostlib::coerce< boost::posix_time::ptime >(t);
     std::stringstream ss;
-    ss.imbue(std::locale(ss.getloc(), new boost::posix_time::time_facet("%a, %d %b %Y %H:%M:%S +0000")));
+    ss.imbue(std::locale(
+        ss.getloc(),
+        new boost::posix_time::time_facet("%a, %d %b %Y %H:%M:%S +0000")
+    ));
     ss << ts;
     return rfc1123_timestamp(ascii_string(ss.str()));
 }
@@ -53,6 +56,11 @@ rfc1123_timestamp fostlib::coercer< rfc1123_timestamp, timestamp >::coerce( time
 timestamp fostlib::coercer< timestamp, json >::coerce( const json &j ) {
     nullable< string > s( j.get< string >() );
     if ( s.isnull() )
-        throw exceptions::not_implemented(L"fostlib::coercer< timestamp, json >::coerce( const json &j ) -- where the JSON is not a string");
-    return timestamp( boost::posix_time::time_from_string( fostlib::coerce< ascii_string >( s.value() ).underlying() ) );
+        throw exceptions::not_implemented(
+            L"fostlib::coercer< timestamp, json >::coerce( const json &j ) -- "
+            L"where the JSON is not a string"
+        );
+    return timestamp( boost::posix_time::time_from_string(
+        fostlib::coerce< ascii_string >( s.value() ).underlying()
+    ) );
 }
