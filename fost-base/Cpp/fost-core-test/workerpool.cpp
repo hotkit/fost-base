3f39c26b9cf0fba8350daa52de5dc86d5cf98ce0
Kirit Sælensminde <k@kirit.com>
2010-02-27 11:05:30 +0000

    The multi-thread test now waits 100ms which hopefully is long enough to always force 2 threads to start.

--- fost-base/Cpp/fost-core-test/workerpool.cpp
+++ fost-base/Cpp/fost-core-test/workerpool.cpp
@@ -1,5 +1,5 @@
 /*
-    Copyright 2009, Felspar Co Ltd. http://fost.3.felspar.com/
+    Copyright 2009-2010, Felspar Co Ltd. http://fost.3.felspar.com/
     Distributed under the Boost Software License, Version 1.0.
     See accompanying file LICENSE_1_0.txt or copy at
         http://www.boost.org/LICENSE_1_0.txt
@@ -20,8 +20,7 @@ FSL_TEST_FUNCTION( construction ) {
 
 namespace {
     int quadrupal( int d ) {
-        for (int c=0; c < d * 16000; ++c )
-            ;
+        boost::this_thread::sleep(boost::posix_time::milliseconds(100));
         return d * 4;
     }
 }
