b8d6e3e5a0e7731a07de172f432ec3a7d719897d
Kirit Sælensminde <k@kirit.com>
2010-02-27 14:24:17 +0000

    Moved most of the date/time library headers to a better location.

--- fost-base/Cpp/include/fost/time.hpp
+++ fost-base/Cpp/include/fost/time.hpp
@@ -1,5 +1,5 @@
 /*
-    Copyright 2000-2009, Felspar Co Ltd. http://fost.3.felspar.com/
+    Copyright 2000-2010, Felspar Co Ltd. http://fost.3.felspar.com/
     Distributed under the Boost Software License, Version 1.0.
     See accompanying file LICENSE_1_0.txt or copy at
         http://www.boost.org/LICENSE_1_0.txt
@@ -12,7 +12,7 @@
 
 
 #include <fost/config.hpp>
-#include <fost/detail/timezone.hpp>
+#include <fost/timezone.hpp>
 
 
 namespace fostlib {
