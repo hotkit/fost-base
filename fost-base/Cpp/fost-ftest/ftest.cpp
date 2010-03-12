e13e07ec7a60d1eeadf10f0e89784ff721890b68
Kirit Sælensminde <k@kirit.com>
2010-03-12 09:33:30 +0000

    Ftest now doesn't try to load .PDB files which are debug database files from MSVC.

--- fost-base/Cpp/fost-ftest/ftest.cpp
+++ fost-base/Cpp/fost-ftest/ftest.cpp
@@ -1,5 +1,5 @@
 /*
-    Copyright 2005-2009, Felspar Co Ltd. http://fost.3.felspar.com/
+    Copyright 2005-2010, Felspar Co Ltd. http://fost.3.felspar.com/
     Distributed under the Boost Software License, Version 1.0.
     See accompanying file LICENSE_1_0.txt or copy at
         http://www.boost.org/LICENSE_1_0.txt
@@ -19,20 +19,21 @@ namespace {
 
 FSL_MAIN(
     L"ftest",
-    L"ftest - Fost test executor\nCopyright (c) 1995-2009, Felspar Co. Ltd."
+    L"ftest - Fost test executor\nCopyright (c) 1995-2010, Felspar Co. Ltd."
 )( fostlib::ostream &out, fostlib::arguments &args ) {
     args.commandSwitch( L"v", L"Tests", L"Display test names" );
     if ( args.size() < 2 )
         throw fostlib::exceptions::null( L"Must specify at least one DLL to load tests for" );
     else {
         std::list< boost::shared_ptr< fostlib::dynlib > > libraries;
-        for ( fostlib::arguments::size_type i( 1 ); i < args.size(); ++i )
-            if ( fostlib::coerce< boost::filesystem::wpath >( args[i].value() ).extension() != L".lib" )
+        for ( fostlib::arguments::size_type i( 1 ); i < args.size(); ++i ) {
+            std::wstring extension = fostlib::coerce< boost::filesystem::wpath >( args[i].value() ).extension();
+            if ( extension != L".lib" && extension != L".pdb" )
                 libraries.push_back( boost::shared_ptr< fostlib::dynlib >( new fostlib::dynlib( args[ i ].value() ) ) );
+        }
         if ( fostlib::test::suite::execute( out ) )
             return 1;
         else
             return 0;
     }
 }
-
