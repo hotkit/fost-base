bc39857ae1049fe532b165bfc0f98dbd304a1613
Kirit Sælensminde <k@kirit.com>
2010-08-15 12:10:03 +0000

    Added new file where we will put the generic push_back members.

---
+++ fost-base/Cpp/fost-core/push_back-tests.cpp
@@ -0,0 +1,15 @@
+/*
+    Copyright 2010, Felspar Co Ltd. http://fost.3.felspar.com/
+    Distributed under the Boost Software License, Version 1.0.
+    See accompanying file LICENSE_1_0.txt or copy at
+        http://www.boost.org/LICENSE_1_0.txt
+*/
+
+
+#include "fost-core-test.hpp"
+
+
+#include <fost/push_back.hpp>
+
+
+FSL_TEST_SUITE( push_back );
