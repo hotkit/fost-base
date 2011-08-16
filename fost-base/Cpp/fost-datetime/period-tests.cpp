7ec41a9d6427142c3e5b5c66f868bf607202914c
Kirit Sælensminde <k@kirit.com>
2011-08-16 03:25:56 +0000

    Removed some empty test functions.

--- fost-base/Cpp/fost-datetime/period-tests.cpp
+++ fost-base/Cpp/fost-datetime/period-tests.cpp
@@ -1,5 +1,5 @@
 /*
-    Copyright 2010, Felspar Co Ltd. http://fost.3.felspar.com/
+    Copyright 2010-2011, Felspar Co Ltd. http://support.felspar.com/
     Distributed under the Boost Software License, Version 1.0.
     See accompanying file LICENSE_1_0.txt or copy at
         http://www.boost.org/LICENSE_1_0.txt
@@ -18,14 +18,13 @@ FSL_TEST_FUNCTION( constructors ) {
     fostlib::test::default_copy_constructable< fostlib::period< int > >();
     fostlib::test::default_copy_constructable< fostlib::period< fostlib::string > >();
     fostlib::period< void > p1(
-        fostlib::timestamp(2010, 1, 1), fostlib::timestamp::now()
-    );
+        fostlib::timestamp(2010, 1, 1), fostlib::timestamp::now());
 }
 
 
-FSL_TEST_FUNCTION( basic ) {
-}
+// FSL_TEST_FUNCTION( basic ) {
+// }
 
 
-FSL_TEST_FUNCTION( json ) {
-}
+// FSL_TEST_FUNCTION( json ) {
+// }
