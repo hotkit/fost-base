d851a459c93e4451e5c871f904b7b517b19900f3
Kirit Sælensminde <k@kirit.com>
2013-01-12 03:53:36 +0000

    Blank header for implementing progress metering.

---
+++ fost-base/Cpp/include/fost/progress.hpp
@@ -0,0 +1,16 @@
+/*
+    Copyright 2013, Felspar Co Ltd. http://support.felspar.com/
+    Distributed under the Boost Software License, Version 1.0.
+    See accompanying file LICENSE_1_0.txt or copy at
+        http://www.boost.org/LICENSE_1_0.txt
+*/
+
+
+#ifndef FOST_PROGRESS_HPP
+#define FOST_PROGRESS_HPP
+#pragma once
+
+
+
+
+#endif // FOST_PROGRESS_HPP
