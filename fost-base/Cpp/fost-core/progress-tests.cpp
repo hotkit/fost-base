0e1d0d48ff802c885277ae54c5d5034308fd3c00
Kirit Sælensminde <k@kirit.com>
2013-01-13 00:32:35 +0000

    Added an empty test.

---
+++ fost-base/Cpp/fost-core/progress-tests.cpp
@@ -0,0 +1,19 @@
+/*
+    Copyright 2013, Felspar Co Ltd. http://support.felspar.com/
+    Distributed under the Boost Software License, Version 1.0.
+    See accompanying file LICENSE_1_0.txt or copy at
+        http://www.boost.org/LICENSE_1_0.txt
+*/
+
+
+#include "fost-core-test.hpp"
+
+#include <fost/progress.hpp>
+
+
+FSL_TEST_SUITE( progress );
+
+
+FSL_TEST_FUNCTION( constructor ) {
+}
+
