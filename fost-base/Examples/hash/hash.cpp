f102fdb538c526c61b6a931654ddea01c2e2eb16
Kirit Sælensminde <k@kirit.com>
2013-01-13 09:02:21 +0000

    Started to implement new file hashing example.

---
+++ fost-base/Examples/hash/hash.cpp
@@ -0,0 +1,18 @@
+/*
+    Copyright 2013, Felspar Co Ltd. http://support.felspar.com/
+    Distributed under the Boost Software License, Version 1.0.
+    See accompanying file LICENSE_1_0.txt or copy at
+        http://www.boost.org/LICENSE_1_0.txt
+*/
+
+
+#include <fost/cli>
+#include <fost/main>
+
+
+FSL_MAIN(
+    L"hash",
+    L"File hashing"
+)( fostlib::ostream &, fostlib::arguments & ) {
+    return 0;
+}
