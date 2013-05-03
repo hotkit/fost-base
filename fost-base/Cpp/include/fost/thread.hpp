465e25ee9ef6893d78a0103d9d3b2f068a3eb3a8
Kirit Sælensminde <k@kirit.com>
2013-05-03 09:21:01 +0000

    Make sure that the `fostlib::worker` can't leak an exception when it's destructed.

--- fost-base/Cpp/include/fost/thread.hpp
+++ fost-base/Cpp/include/fost/thread.hpp
@@ -1,5 +1,5 @@
 /*
-    Copyright 1997-2012, Felspar Co Ltd. http://support.felspar.com/
+    Copyright 1997-2013, Felspar Co Ltd. http://support.felspar.com/
     Distributed under the Boost Software License, Version 1.0.
     See accompanying file LICENSE_1_0.txt or copy at
         http://www.boost.org/LICENSE_1_0.txt
@@ -31,7 +31,7 @@ namespace fostlib {
         /// Start a worker ready to accept new jobs
         worker();
         /// Terminate the worker, waiting for the current job to complete
-        ~worker();
+        ~worker() throw ();
 
         boost::shared_ptr< detail::future_result< void > > operator()
             ( boost::function0< void > f );
