8a546b83b087c056ac8db184e9d0fafbfab830be
Kirit Sælensminde <k@kirit.com>
2014-01-14 04:37:32 +0000

    Silence a warning for g++ in the depths of the Boost code.

--- fost-base/Cpp/include/fost/config.hpp
+++ fost-base/Cpp/include/fost/config.hpp
@@ -1,5 +1,5 @@
 /*
-    Copyright 2001-2009, Felspar Co Ltd. http://fost.3.felspar.com/
+    Copyright 2001-2014, Felspar Co Ltd. http://support.felspar.com/
     Distributed under the Boost Software License, Version 1.0.
     See accompanying file LICENSE_1_0.txt or copy at
         http://www.boost.org/LICENSE_1_0.txt
@@ -73,6 +73,8 @@
 #if INT_MAX != LONG_MAX && LONG_MAX != LLONG_MAX
     #define FOST_USE_LONG
 #endif
+
+#pragma GCC diagnostic ignored "-Wunused-local-typedefs"
 
 #include <boost/utility.hpp>
 #include <boost/smart_ptr.hpp>
