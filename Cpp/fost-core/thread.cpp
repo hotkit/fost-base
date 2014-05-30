bf7a1450420ee6604f66a6e052f07245cba838e0
Kirit Sælensminde <k@kirit.com>
2014-05-30 16:32:11 +0700

    Add a missing header.

--- Cpp/fost-core/thread.cpp
+++ Cpp/fost-core/thread.cpp
@@ -1,5 +1,5 @@
 /*
-    Copyright 1997-2013, Felspar Co Ltd. http://support.felspar.com/
+    Copyright 1997-2014, Felspar Co Ltd. http://support.felspar.com/
     Distributed under the Boost Software License, Version 1.0.
     See accompanying file LICENSE_1_0.txt or copy at
         http://www.boost.org/LICENSE_1_0.txt
@@ -13,6 +13,8 @@
 #include <boost/bind.hpp>
 #include <boost/lambda/bind.hpp>
 #include <boost/lambda/lambda.hpp>
+
+#include <utility>
 
 
 using namespace fostlib;
