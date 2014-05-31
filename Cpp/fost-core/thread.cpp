4d484a976b3a5b01fad76c4ab47b45a48346fdbb
Kirit Sælensminde <k@kirit.com>
2014-05-31 09:29:41 +0700

    Redesign the thread handling.

--- Cpp/fost-core/thread.cpp
+++ Cpp/fost-core/thread.cpp
@@ -9,6 +9,7 @@
 #include "fost-core.hpp"
 #include <fost/counter.hpp>
 #include <fost/thread.hpp>
+#include <fost/log.hpp>
 
 #include <boost/bind.hpp>
 #include <boost/lambda/bind.hpp>
@@ -34,45 +35,57 @@ namespace {
 }
 
 
+struct fostlib::worker::context {
+    context();
+
+    typedef std::list< std::pair<
+        boost::shared_ptr< detail::future_result< void > >,
+        boost::function0< void > > > t_queue;
+    t_queue m_queue;
+
+    bool m_terminate;
+
+    boost::mutex m_mutex;
+    boost::condition m_control;
+    boost::scoped_ptr<boost::thread> m_thread;
+
+    static void execute(boost::shared_ptr<context> self);
+};
+
+
 /*
     fostlib::worker
 */
 
 
 fostlib::worker::worker()
-: m_terminate(false), m_thread(boost::bind(&worker::execute, this)) {
+: self(new context) {
+    self->m_thread.reset(
+        new boost::thread(
+            boost::bind(&context::execute, self)));
     ++g_workers();
-}
-
-
-/*
-    This move constructor is awkward. Because the `right` worker is also
-    accessed by another thread we have to be careful about what we're
-    doing here. In practice we have to drop the old thread and make a new
-    one -- nothing else is guaranteed to be safe.
-
-    A better design will replace the thread, mutex and condition variable
-    with smart pointers so that they can be moved whilst leaving the rest
-    of the object in tact. We'd also need to avoid use of the `this` pointer,
-    so the `execute` member is out of the question as well.
-*/
-fostlib::worker::worker(worker &&right)
-: m_terminate(false), m_thread(boost::bind(&worker::execute, this)) {
-    ++g_workers();
-    boost::mutex::scoped_lock left_lock(m_mutex);
-    boost::mutex::scoped_lock right_lock(m_mutex);
-    m_queue = std::move(right.m_queue);
 }
 
 
 fostlib::worker::~worker() throw ()
 try {
     {
-        boost::mutex::scoped_lock lock( m_mutex );
-        m_terminate = true;
-        m_control.notify_all();
+        boost::mutex::scoped_lock lock(self->m_mutex);
+        self->m_terminate = true;
+        self->m_control.notify_all();
     }
-    m_thread.join();
+    if ( boost::this_thread::get_id() != self->m_thread->get_id() ) {
+        /*
+            It is possible for a thread to commit suicide, in which case
+            there is nothing we want to notify and we certainly don't want
+            to join it.
+
+            It is far more usual that the thread is destructed from another
+            thread (most often the one that created it). In this case we do
+            want to shut the thread down normally.
+        */
+        self->m_thread->join();
+    }
     --g_workers();
 } catch ( ... ) {
     absorb_exception();
@@ -82,7 +95,7 @@ try {
 boost::shared_ptr< fostlib::detail::future_result< void > > fostlib::worker::operator()
         ( boost::function0< void > f ) {
     boost::shared_ptr< detail::future_result< void > > future( new detail::future_result< void > );
-    queue( future, f );
+    queue(future, f);
     return future;
 }
 
@@ -90,59 +103,75 @@ boost::shared_ptr< fostlib::detail::future_result< void > > fostlib::worker::ope
  void fostlib::worker::queue(
      boost::shared_ptr< detail::future_result< void > > future, boost::function0< void > f
  ) const {
-    boost::mutex::scoped_lock lock( m_mutex );
-    m_queue.push_back( std::make_pair( future, f ) );
-    m_control.notify_all();
-}
-
-
-void fostlib::worker::execute() {
-    fostlib::exceptions::structured_handler handler;
-#ifdef FOST_OS_WINDOWS
-    com_hr( ::CoInitializeEx( NULL, COINIT_APARTMENTTHREADED ), L"CoInitializeEx at start of fostlib::worker thread" );
-#endif
-    bool terminate;
-    do {
-        t_queue job;
-        { // Find a job to perform
-            boost::mutex::scoped_lock lock( m_mutex );
-            terminate = m_terminate;
-            if ( !terminate && m_queue.empty() )
-                m_control.wait( lock );
-            job.swap( m_queue );
-        }
-        for ( t_queue::const_iterator j( job.begin() ); j != job.end(); ++j ) {
-            // Execute job
-            try {
-                const t_queue::value_type &job = *j;
-                if ( terminate )
-                    job.first->m_exception = L"Thread terminated";
-                else
-                    job.second();
-            } catch ( fostlib::exceptions::exception &e ) {
-                boost::mutex::scoped_lock lock( j->first->m_mutex );
-                j->first->m_exception = coerce< fostlib::string >( e );
-            } catch ( ... ) {
-                boost::mutex::scoped_lock lock( j->first->m_mutex );
-                j->first->m_exception = L"An unknown exception was caught";
-                terminate = true; // Kill the thread after an unknown exception
-            }
-            {// Notify futures
-                boost::mutex::scoped_lock lock( j->first->m_mutex );
-                j->first->m_completed = true;
-                j->first->m_has_result.notify_all();
-            }
-            if ( !terminate ) {
-                boost::mutex::scoped_lock lock( m_mutex );
-                terminate = m_terminate;
-            }
-        }
-    } while ( !terminate );
+    boost::mutex::scoped_lock lock(self->m_mutex);
+    self->m_queue.push_back(std::make_pair(future, f));
+    self->m_control.notify_all();
 }
 
 
 int fostlib::worker::workers() {
     return g_workers().value();
+}
+
+
+/*
+    fostlib::worker::context
+*/
+
+
+fostlib::worker::context::context()
+: m_terminate(false) {
+}
+
+
+void fostlib::worker::context::execute(boost::shared_ptr<context> self) {
+    fostlib::exceptions::structured_handler handler;
+#ifdef FOST_OS_WINDOWS
+    com_hr( ::CoInitializeEx( NULL, COINIT_APARTMENTTHREADED ), L"CoInitializeEx at start of fostlib::worker thread" );
+#endif
+    bool terminate = false;
+    do {
+        t_queue job;
+        { // Find a job to perform
+            boost::mutex::scoped_lock lock(self->m_mutex);
+            terminate = self->m_terminate;
+            if ( !terminate && self->m_queue.empty() ) {
+                self->m_control.wait(lock);
+            }
+            job.swap(self->m_queue);
+        }
+        for ( t_queue::const_iterator j(job.begin()); j != job.end(); ++j ) {
+            // Execute job
+            try {
+                const t_queue::value_type &job = *j;
+                if ( terminate ) {
+                    job.first->m_exception = L"Thread terminated";
+                } else {
+                    job.second();
+                }
+            } catch ( fostlib::exceptions::exception &e ) {
+                boost::mutex::scoped_lock lock(j->first->m_mutex);
+                j->first->m_exception = coerce< fostlib::string >(e);
+            } catch ( std::exception &e ) {
+                boost::mutex::scoped_lock lock(j->first->m_mutex);
+                j->first->m_exception = coerce< fostlib::string >(e.what());
+            } catch ( ... ) {
+                log::error("An unknown exception was caught -- abandoning thread");
+                boost::mutex::scoped_lock lock(j->first->m_mutex);
+                j->first->m_exception = L"An unknown exception was caught";
+                terminate = true; // Kill the thread after an unknown exception
+            }
+            {// Notify futures
+                boost::mutex::scoped_lock lock(j->first->m_mutex);
+                j->first->m_completed = true;
+                j->first->m_has_result.notify_all();
+            }
+            if ( !terminate ) {
+                boost::mutex::scoped_lock lock(self->m_mutex);
+                terminate = self->m_terminate;
+            }
+        }
+    } while ( !terminate );
 }
 
 
