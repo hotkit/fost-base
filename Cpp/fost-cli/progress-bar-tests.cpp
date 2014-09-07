aa54684e6276d5139d8e54ef0ec71e7692e626de
Kirit Sælensminde <k@kirit.com>
2014-09-07 14:26:23 +0700

    Fixed the progress bar tests.

--- Cpp/fost-cli/progress-bar-tests.cpp
+++ Cpp/fost-cli/progress-bar-tests.cpp
@@ -1,5 +1,5 @@
 /*
-    Copyright 2013, Felspar Co Ltd. http://support.felspar.com/
+    Copyright 2013-2014, Felspar Co Ltd. http://support.felspar.com/
     Distributed under the Boost Software License, Version 1.0.
     See accompanying file LICENSE_1_0.txt or copy at
         http://www.boost.org/LICENSE_1_0.txt
@@ -19,12 +19,12 @@ FSL_TEST_FUNCTION(bar) {
     FSL_CHECK_EQ("----------",
         fostlib::cli::bar(fostlib::meter::reading(false), 10));
     FSL_CHECK_EQ("----------",
-        fostlib::cli::bar(fostlib::meter::reading(false, 0, 0), 10));
+        fostlib::cli::bar(fostlib::meter::reading(fostlib::json(), false, 0, 0), 10));
     FSL_CHECK_EQ("          ",
-        fostlib::cli::bar(fostlib::meter::reading(false, 0, 10), 10));
+        fostlib::cli::bar(fostlib::meter::reading(fostlib::json(), false, 0, 10), 10));
     FSL_CHECK_EQ("...       ",
-        fostlib::cli::bar(fostlib::meter::reading(false, 3, 10), 10));
+        fostlib::cli::bar(fostlib::meter::reading(fostlib::json(), false, 3, 10), 10));
     FSL_CHECK_EQ(".......   ",
-        fostlib::cli::bar(fostlib::meter::reading(false, 7, 10), 10));
+        fostlib::cli::bar(fostlib::meter::reading(fostlib::json(), false, 7, 10), 10));
 }
 
