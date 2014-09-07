31f34c8389a5d97e5c972480cb64aea13da1b5df
Kirit Sælensminde <k@kirit.com>
2014-09-07 13:53:03 +0700

    There is a new `fostlib::cli::monitor` function.
    It can be used to display a progress bar monitoring work towards a
    future.

--- Cpp/include/fost/progress-bar-cli.hpp
+++ Cpp/include/fost/progress-bar-cli.hpp
@@ -1,5 +1,5 @@
 /*
-    Copyright 2013, Felspar Co Ltd. http://support.felspar.com/
+    Copyright 2013-2014, Felspar Co Ltd. http://support.felspar.com/
     Distributed under the Boost Software License, Version 1.0.
     See accompanying file LICENSE_1_0.txt or copy at
         http://www.boost.org/LICENSE_1_0.txt
@@ -20,9 +20,33 @@ namespace fostlib {
     namespace cli {
 
 
+        FOST_CLI_DECLSPEC
         /// Return a string of the requested length representing the reading
-        FOST_CLI_DECLSPEC string bar(const meter::reading &, std::size_t width);
+        string bar(const meter::reading &, std::size_t width);
 
+        /// Monitor a future and report progress
+        template<typename R, typename P, typename S, typename T = fostlib::milliseconds>
+        void monitor(
+            fostlib::ostream &out,
+            meter &tracking, R &future,
+            S suffix = [] (const fostlib::meter::reading &) {
+                return string("[");
+            },
+            P prefix = [] (const fostlib::meter::reading &) {
+                return string("]");
+            },
+            const T delay = fostlib::milliseconds(50),
+            const std::size_t pips = 50
+        ) {
+            while ( !future.available(delay) ) {
+                fostlib::meter::reading current(tracking());
+                out << prefix(current) << bar(current, pips)
+                    << suffix(current) << '\r' << std::flush;
+            }
+            fostlib::meter::reading current(tracking());
+            out << prefix(current) << bar(current, pips)
+                << suffix(current) << std::endl;
+        }
 
     }
 
