31366d5226d39ed6d8f41ad98222f5774fd29755
Kirit Sælensminde <k@kirit.com>
2015-05-30 13:38:37 +0700

    Added colour options to the `stdout` logger.

--- Cpp/fost-cli/logger-stdio.cpp
+++ Cpp/fost-cli/logger-stdio.cpp
@@ -1,5 +1,5 @@
 /*
-    Copyright 2010-2014, Felspar Co Ltd. http://support.felspar.com/
+    Copyright 2010-2015, Felspar Co Ltd. http://support.felspar.com/
     Distributed under the Boost Software License, Version 1.0.
     See accompanying file LICENSE_1_0.txt or copy at
         http://www.boost.org/LICENSE_1_0.txt
@@ -14,12 +14,15 @@
 
 namespace {
     class ostream_logger {
-        std::size_t log_level;
+        const std::size_t log_level;
+        const bool colour;
         public:
-            ostream_logger(const fostlib::json &conf) {
-                log_level = fostlib::coerce< fostlib::nullable<int> >(
+            ostream_logger(const fostlib::json &conf)
+            : log_level(fostlib::coerce<fostlib::nullable<int>>(
                         conf["log-level"]).value(
-                    fostlib::log::error_level_tag::level());
+                            fostlib::log::error_level_tag::level())),
+                    colour(fostlib::coerce<fostlib::nullable<bool>>(
+                        conf["color"]).value(false)) {
             }
             bool operator () ( const fostlib::log::message &m ) {
 #ifdef FOST_OS_WINDOWS
@@ -27,12 +30,28 @@ namespace {
 #else
     #define COUT std::cout
 #endif
+                if ( colour ) {
+                    if ( m.level() <= fostlib::log::debug_level_tag::level() ) {
+                        COUT << "\33[0;37m";
+                    } else if ( m.level() <= fostlib::log::info_level_tag::level() ) {
+                        COUT << "\33[0;32m";
+                    } else if ( m.level() <= fostlib::log::warning_level_tag::level() ) {
+                        COUT << "\33[0;33m";
+                    } else if ( m.level() <= fostlib::log::error_level_tag::level() ) {
+                        COUT << "\33[0;31m";
+                    } else {
+                        COUT << "\33[1;31m";
+                    }
+                }
                 if ( m.level() >= log_level ) {
                     COUT << m.when() << " " << m.name();
                     if ( !m.module().isnull() ) {
                         COUT<< " " << m.module().value();
                     }
                     COUT<< '\n' << m.body() << std::endl;
+                }
+                if ( colour ) {
+                    COUT << "\33[0;39m";
                 }
                 return true;
             }
