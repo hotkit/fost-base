abec4ad96f87d687e152da96a417aaf943b52b3c
Kirit Sælensminde <k@kirit.com>
2015-12-10 14:26:01 +0700

    Initial skeleton for implementing PBKDF2.

---
+++ Cpp/fost-crypto/password.cpp
@@ -0,0 +1,16 @@
+/*
+    Copyright 2015, Felspar Co Ltd. http://support.felspar.com/
+    Distributed under the Boost Software License, Version 1.0.
+    See accompanying file LICENSE_1_0.txt or copy at
+        http://www.boost.org/LICENSE_1_0.txt
+*/
+
+
+#include "fost-crypto.hpp"
+#include <fost/password.hpp>
+
+
+std::array<unsigned char, 32> fostlib::pbkdf2_sha2() {
+    throw fostlib::exceptions::not_implemented(__FUNCTION__);
+}
+
---
+++ Cpp/include/fost/password.hpp
@@ -0,0 +1,22 @@
+/*
+    Copyright 2015, Felspar Co Ltd. http://support.felspar.com/
+    Distributed under the Boost Software License, Version 1.0.
+    See accompanying file LICENSE_1_0.txt or copy at
+        http://www.boost.org/LICENSE_1_0.txt
+*/
+
+
+#pragma once
+
+
+#include <fost/core>
+
+
+namespace fostlib {
+
+
+    std::array<unsigned char, 32> pbkdf2_sha2();
+
+
+}
+
