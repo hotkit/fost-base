1d8ed4bfbf1a8e3b8577d7fd60670b4e70f58b53
Kirit Sælensminde <k@kirit.com>
2016-03-22 18:10:57 +0700

    Added coercion for `std::vector` to JSON

--- Cpp/include/fost/json.hpp
+++ Cpp/include/fost/json.hpp
@@ -1,5 +1,5 @@
 /*
-    Copyright 2007-2015, Felspar Co Ltd. http://support.felspar.com/
+    Copyright 2007-2016, Felspar Co Ltd. http://support.felspar.com/
     Distributed under the Boost Software License, Version 1.0.
     See accompanying file LICENSE_1_0.txt or copy at
         http://www.boost.org/LICENSE_1_0.txt
@@ -321,8 +321,20 @@ namespace fostlib {
     };
 
 
-}
+    /// Allow coerceion from a std::vector of something coerceable
+    /// to JSON
+    template<typename V>
+    struct coercer<json, std::vector<V>> {
+        json coerce(const std::vector<V> &v) {
+            json::array_t a;
+            for ( const auto &d : v )
+                a.push_back(boost::make_shared<json>(fostlib::coerce<json>(d)));
+            return a;
+        }
+    };
 
+
+}
 
 
 namespace std {
