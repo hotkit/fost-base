a14d4e8843a2b88c0b35c9bad00f08410690bf6c
Kirit Sælensminde <k@kirit.com>
2016-07-29 14:04:37 +0700

    Allow a wider range of types for extra information to the constructor.
    Extra information for the `not_implemented` constructor can now be
    anything coercible to JSON. Also remove the exception from the
    `fost/core` header.

--- Cpp/include/fost/exception/not_implemented.hpp
+++ Cpp/include/fost/exception/not_implemented.hpp
@@ -1,5 +1,5 @@
 /*
-    Copyright  2001-2016, Felspar Co Ltd. http://support.felspar.com/
+    Copyright 2001-2016, Felspar Co Ltd. http://support.felspar.com/
     Distributed under the Boost Software License, Version 1.0.
     See accompanying file LICENSE_1_0.txt or copy at
         http://www.boost.org/LICENSE_1_0.txt
@@ -9,6 +9,7 @@
 #pragma once
 
 
+#include <fost/coerce.hpp>
 #include <fost/exception.hpp>
 
 
@@ -34,6 +35,12 @@ namespace fostlib {
             /// Throw providing a functio name, message and extra JSON information
             not_implemented(
                 const string &function, const string &message, const json &extra);
+            /// Extra information can be anything coercible to JSON
+            template<typename E>
+            not_implemented(
+                const string &function, const string &message, const E &extra)
+            : not_implemented(function, message, coerce<json>(extra)) {
+            }
             /// Allow us to throw from a Boost error code
             not_implemented(
                 const string &function, boost::system::error_code error
