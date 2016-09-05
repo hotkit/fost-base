2e2c3b214fcfec9f3c2297aff20216f32147c39c
Kirit Sælensminde <k@kirit.com>
2016-09-05 12:00:40 +0700

    Correctly coerce to UTF16 when making a std::wstring.
    This isn't ideal.

--- Cpp/fost-core/coerce-linux.cpp
+++ Cpp/fost-core/coerce-linux.cpp
@@ -1,5 +1,5 @@
 /*
-    Copyright 2002-2008, Felspar Co Ltd. http://fost.3.felspar.com/
+    Copyright 2002-2016, Felspar Co Ltd. http://fost.3.felspar.com/
     Distributed under the Boost Software License, Version 1.0.
     See accompanying file LICENSE_1_0.txt or copy at
         http://www.boost.org/LICENSE_1_0.txt
@@ -29,9 +29,22 @@ string fostlib::coercer< string, double >::coerce( double f ) {
 
 
 std::wstring fostlib::coercer< std::wstring, string >::coerce( const string &s ) {
-    return std::wstring( s.begin(), s.end() );
+    // TODO: Don't use this type as a proxy for utf16 strings. We can have
+    // a proper tagged string now.
+    std::wstring res;
+    res.reserve(s.length());
+    utf16 u16b[2];
+    for ( auto ch : s ) {
+        utf::encode(ch, u16b, u16b + 2);
+        res += u16b[0];
+        if ( utf::utf16length(ch) == 2 ) {
+            res += u16b[1];
+        }
+    }
+    return res;
 }
 string fostlib::coercer< string, std::wstring >::coerce( const std::wstring &s ) {
+    // TODO: This doesn't UTF16 decode properly
     string r;
     r.reserve( s.length() );
     for ( std::wstring::const_iterator p( s.begin() ); p != s.end(); ++p )
