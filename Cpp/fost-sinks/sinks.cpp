ef5616be336e8f79b2948f9b4f17c2b030cd7028
Kirit Sælensminde <k@kirit.com>
2016-11-02 12:40:20 +0700

    Add default location for the file sinks directory.

--- Cpp/fost-sinks/sinks.cpp
+++ Cpp/fost-sinks/sinks.cpp
@@ -11,3 +11,8 @@
 
 const fostlib::module fostlib::c_fost_sinks(c_fost, "sinks");
 
+
+const fostlib::setting<fostlib::string> c_log_sink_file_root(
+    "sinks.cpp", "Log sinks", "Log files directory",
+    "var/log/fost", true);
+
--- Cpp/include/fost/sinks.hpp
+++ Cpp/include/fost/sinks.hpp
@@ -15,7 +15,12 @@
 namespace fostlib {
 
 
+    /// The fost-sinks module
     extern const module c_fost_sinks;
+
+
+    /// Location for the file sinks
+    extern const fostlib::setting<fostlib::string> c_log_sink_file_root;
 
 
 }
