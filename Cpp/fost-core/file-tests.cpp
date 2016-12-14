5cb883094436ea76ef84bea2a90aba35d05aef07
Kirit Sælensminde <k@kirit.com>
2016-12-14 11:22:20 +0700

    Add error detection into `save_file`.

--- Cpp/fost-core/file-tests.cpp
+++ Cpp/fost-core/file-tests.cpp
@@ -1,5 +1,5 @@
 /*
-    Copyright 2013-2015, Felspar Co Ltd. http://support.felspar.com/
+    Copyright 2013-2016, Felspar Co Ltd. http://support.felspar.com/
     Distributed under the Boost Software License, Version 1.0.
     See accompanying file LICENSE_1_0.txt or copy at
         http://www.boost.org/LICENSE_1_0.txt
@@ -9,15 +9,10 @@
 #include "fost-core-test.hpp"
 #include <fost/file>
 
+#include <fost/exception/file_error.hpp>
+
 
 FSL_TEST_SUITE(file);
-
-
-FSL_TEST_FUNCTION(unique_filename) {
-    boost::filesystem::path path;
-    FSL_CHECK_NOTHROW(path = fostlib::unique_filename());
-    FSL_CHECK(!boost::filesystem::exists(path));
-}
 
 
 FSL_TEST_FUNCTION(join_paths) {
@@ -35,5 +30,21 @@ FSL_TEST_FUNCTION(join_paths) {
 //     FSL_CHECK_EQ(
 //         fostlib::join_paths("path", "C:\\something"), "C:\\something");
 // #endif
+}
+
+
+FSL_TEST_FUNCTION(unique_filename) {
+    boost::filesystem::path path;
+    FSL_CHECK_NOTHROW(path = fostlib::unique_filename());
+    FSL_CHECK(!boost::filesystem::exists(path));
+}
+
+
+FSL_TEST_FUNCTION(save_file) {
+    const boost::filesystem::path filename = "/not-allowed.txt";
+    FSL_CHECK_EXCEPTION(
+        fostlib::utf::save_file(filename, "some text"),
+        fostlib::exceptions::file_error&);
+    FSL_CHECK(not boost::filesystem::exists(filename));
 }
 
