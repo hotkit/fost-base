5161f3dfc1f2de0e71c9f276078589a256ec23b3
Kirit Sælensminde <k@kirit.com>
2017-01-25 17:02:58 +0700

    Add support for stderr to the stdout logger.

--- Cpp/fost-cli/logger-stdio.cpp
+++ Cpp/fost-cli/logger-stdio.cpp
@@ -12,42 +12,37 @@
 #include <iostream>
 
 
-#ifdef FOST_OS_WINDOWS
-    #define COUT std::wcout
-#else
-    #define COUT std::cout
-#endif
-
-
 namespace {
     struct disp {
         typedef void result_type;
+        std::ostream &channel;
+
         template<typename T>
         void operator () (const T &t) const {
-            COUT << ' ' << t << '\n' << std::endl;
+            channel << ' ' << t << '\n' << std::endl;
         }
         void operator () (const fostlib::string &s) const {
-            COUT << s << std::endl;
+            channel << s << std::endl;
         }
         void operator () (const fostlib::json::object_p &o) const {
             if ( o->find("") != o->end() ) {
                 fostlib::json copy = *o;
                 fostlib::string m = fostlib::coerce<fostlib::string>(copy[""]);
                 fostlib::jcursor("").del_key(copy);
-                COUT << ' ' << m << '\n' << copy << std::endl;
+                channel << ' ' << m << '\n' << copy << std::endl;
             } else {
-                COUT << '\n' << *o << std::endl;
+                channel << '\n' << *o << std::endl;
             }
         }
         void operator () (const fostlib::json::array_p &a) const {
             if ( a->size() > 0 && (*a)[0].isatom() ) {
-                COUT << ' ' << fostlib::coerce<fostlib::string>((*a)[0]) << '\n';
+                channel << ' ' << fostlib::coerce<fostlib::string>((*a)[0]) << '\n';
                 for ( std::size_t i(1); i != a->size(); ++i ) {
-                    COUT << (*a)[i] << '\n';
+                    channel << (*a)[i] << '\n';
                 }
-                COUT << std::endl;
+                channel << std::endl;
             } else {
-                COUT << '\n' << *a << std::endl;
+                channel << '\n' << *a << std::endl;
             }
         }
     };
@@ -55,39 +50,42 @@ namespace {
     class ostream_logger {
         const std::size_t log_level;
         const bool colour;
+        std::ostream &channel;
     public:
         ostream_logger(const fostlib::json &conf)
         : log_level(fostlib::coerce<fostlib::nullable<int>>(
-                    conf["log-level"]).value_or(
-                        fostlib::log::error_level_tag::level())),
-                colour(fostlib::coerce<fostlib::nullable<bool>>(
-                    conf["color"]).value_or(false)) {
+            conf["log-level"]).value_or(
+                fostlib::log::error_level_tag::level())),
+            colour(fostlib::coerce<fostlib::nullable<bool>>(
+                conf["color"]).value_or(false)),
+            channel(conf["channel"] == fostlib::json("stderr") ? std::cerr : std::cout)
+        {
         }
         bool operator () ( const fostlib::log::message &m ) {
             if ( colour ) {
                 if ( m.level() <= fostlib::log::debug_level_tag::level()  + 0x100 ) {
-                    COUT << "\33[0;37m";
+                    channel << "\33[0;37m";
                 } else if ( m.level() <= fostlib::log::info_level_tag::level() + 0x300 ) {
-                    COUT << "\33[0;32m";
+                    channel << "\33[0;32m";
                 } else if ( m.level() <= fostlib::log::warning_level_tag::level() + 0x2000 ) {
-                    COUT << "\33[1;33m";
+                    channel << "\33[1;33m";
                 } else if ( m.level() <= fostlib::log::error_level_tag::level() + 0x20000 ) {
-                    COUT << "\33[0;31m";
+                    channel << "\33[0;31m";
                 } else {
-                    COUT << "\33[1;31m";
+                    channel << "\33[1;31m";
                 }
             }
             if ( m.level() >= log_level ) {
-                COUT << m.when() << " " << m.name() << " " << m.module();
+                channel << m.when() << " " << m.name() << " " << m.module();
                 if ( colour ) {
-                    disp d;
+                    disp d{channel};
                     m.body().apply_visitor(d);
                 } else {
-                    COUT<< '\n' << m.body() << std::endl;
+                    channel<< '\n' << m.body() << std::endl;
                 }
             }
             if ( colour ) {
-                COUT << "\33[0;39m";
+                channel << "\33[0;39m";
             }
             return true;
         }
