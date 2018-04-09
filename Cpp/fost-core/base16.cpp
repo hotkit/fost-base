201b22457287c684fe225b67f1ba247792642600
Kirit Sælensminde <k@kirit.com>
2018-04-09 12:07:53 +0700

    Add some basic base16 support

---
+++ Cpp/fost-core/base16.cpp
@@ -0,0 +1,63 @@
+/*
+    Copyright 2018, Felspar Co Ltd. http://support.felspar.com/
+    Distributed under the Boost Software License, Version 1.0.
+    See accompanying file LICENSE_1_0.txt or copy at
+        http://www.boost.org/LICENSE_1_0.txt
+*/
+
+
+#include "fost-core.hpp"
+#include <fost/base16.hpp>
+#include <fost/exception/parse_error.hpp>
+
+
+namespace {
+
+
+    const char *b16chars = "0123456789ABCDEF";
+
+
+}
+
+
+void fostlib::base16_string_tag::do_encode(nliteral l, ascii_string &s) {
+    s = l;
+    check_encoded(s);
+}
+
+
+void fostlib::base16_string_tag::do_encode(const ascii_string &l, ascii_string &s) {
+    s = l;
+    check_encoded(s);
+}
+
+
+void fostlib::base16_string_tag::check_encoded(const fostlib::ascii_string &s) {
+    if ( s.underlying().find_first_not_of(b16chars) != std::string::npos )
+        throw fostlib::exceptions::parse_error(
+            "Non base16 character found in string", coerce<string>(s));
+}
+
+
+std::vector<unsigned char> fostlib::coercer<std::vector<unsigned char>, fostlib::base16_string>::coerce(
+    const base16_string &s
+) {
+    if ( s.length() & 1 ) throw fostlib::exceptions::parse_error(
+            "Base16 data must contain whole bytes, "
+            "i.e. an even number of characters");
+    std::vector<unsigned char> v;
+    std::size_t p{4}, b{};
+    for ( auto c : s.underlying().underlying() ) {
+        const auto ch = std::find(b16chars, b16chars + 16, c) - b16chars;
+        b += ch << p;
+        if ( not p ) {
+            v.push_back(b);
+            b = 0;
+            p = 4;
+        } else {
+            p = 0;
+        }
+    }
+    return v;
+}
+
---
+++ Cpp/fost-core/base16.tests.cpp
@@ -0,0 +1,30 @@
+/*
+    Copyright 2018, Felspar Co Ltd. http://support.felspar.com/
+    Distributed under the Boost Software License, Version 1.0.
+    See accompanying file LICENSE_1_0.txt or copy at
+        http://www.boost.org/LICENSE_1_0.txt
+*/
+
+
+#include <fost/base16>
+#include <fost/test>
+
+
+FSL_TEST_SUITE(base16);
+
+
+FSL_TEST_FUNCTION(to_bytes) {
+    fostlib::base16_string b1{"1234"};
+    auto v1 = fostlib::coerce<std::vector<unsigned char>>(b1);
+    FSL_CHECK_EQ(v1[0], 0x12);
+    FSL_CHECK_EQ(v1[1], 0x34);
+}
+
+
+FSL_TEST_FUNCTION(odd_char_count) {
+    fostlib::base16_string b1{"123"};
+    FSL_CHECK_EXCEPTION(
+        fostlib::coerce<std::vector<unsigned char>>(b1),
+        fostlib::exceptions::parse_error&);
+}
+
