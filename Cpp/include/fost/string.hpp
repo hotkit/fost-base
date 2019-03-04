c26a1aa82ef0d6a543208aaacf736177b4b4cda4
Kirit Sælensminde <kirit@kirit.com>
2019-03-04 19:43:45 +0700

    Remove use of deprecated function

--- Cpp/include/fost/string.hpp
+++ Cpp/include/fost/string.hpp
@@ -168,7 +168,7 @@ namespace fostlib {
         string substr() const { return *this; }
         string substr(size_type b) const { return u8string::substr(b); }
         string substr(size_type b, size_type c) const {
-            return u8string::substr(b, b + c);
+            return u8string::substr_pos(b, b + c);
         }
         char32_t at(size_type) const;
         char32_t operator[](size_type p) const { return at(p); }
