4053cb8694a6d1d65593e663ac71ad7d66f07540
Kirit Sælensminde <kirit@kirit.com>
2019-03-12 11:09:19 +0700

    Describe how this is supposed to work.

--- Cpp/fost-core/dynlib.cpp
+++ Cpp/fost-core/dynlib.cpp
@@ -1,5 +1,5 @@
 /**
-    Copyright 2008-2018, Felspar Co Ltd. <http://support.felspar.com/>
+    Copyright 2008-2019, Felspar Co Ltd. <http://support.felspar.com/>
 
     Distributed under the Boost Software License, Version 1.0.
     See <http://www.boost.org/LICENSE_1_0.txt>
@@ -58,6 +58,10 @@ struct fostlib::dynlib::impl {
 fostlib::dynlib::dynlib(const string &lib) : m_lib(nullptr) {
     fostlib::json attempts;
     const auto tryload = [&attempts](string dl) {
+        /**
+         * Using `RTLD_NODELETE` may seem like a good idea here, but it
+         * turns out that things don't work at all well with that.
+         */
         void *h = dlopen(dl.c_str(), RTLD_NOW);
         if (not h)
             fostlib::insert(
@@ -90,10 +94,10 @@ fostlib::dynlib::dynlib(const string &lib) : m_lib(nullptr) {
 fostlib::dynlib::~dynlib() {
     /**
         We don't want to actually unload the .so yet as there may well still be
-       objects that it manages.
+        objects that it manages.
 
-        The choice of adding the deletion to `atexit` here is somewhat
-       arbitrary. It could have just as easily gone in the constructor
+        By putting the deletion in the `atexit` handler we should ensure that
+        the libraries are unloaded in the right order (that of the destructors).
     */
     if (m_lib) fostlib::atexit([libp = m_lib]() { delete libp; });
 }
