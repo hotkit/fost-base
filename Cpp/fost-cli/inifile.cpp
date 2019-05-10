9e23c95e3b6e772ecbb21b6c26edb274ed81a234
Kirit Sælensminde <kirit@kirit.com>
2019-05-10 15:52:12 +0700

    Remove old APIs in ini file parsing.

--- Cpp/fost-cli/inifile.cpp
+++ Cpp/fost-cli/inifile.cpp
@@ -1,5 +1,5 @@
 /**
-    Copyright 1998-2018, Felspar Co Ltd. <http://support.felspar.com/>
+    Copyright 1998-2019, Felspar Co Ltd. <http://support.felspar.com/>
 
     Distributed under the Boost Software License, Version 1.0.
     See <http://www.boost.org/LICENSE_1_0.txt>
@@ -23,7 +23,7 @@ using namespace fostlib;
 fostlib::exceptions::invalid_ini_line::invalid_ini_line(
         const fostlib::string &section,
         const fostlib::string &read,
-        const fostlib::string &processed) throw()
+        const fostlib::string &processed) noexcept
 : exception() {
     fostlib::insert(data(), "section", section);
     fostlib::insert(data(), "ini-file", read);
@@ -32,7 +32,7 @@ fostlib::exceptions::invalid_ini_line::invalid_ini_line(
 
 
 const wchar_t *const fostlib::exceptions::invalid_ini_line::message() const
-        throw() {
+        noexcept {
     return L"Invalid line in INI file";
 }
 
@@ -42,8 +42,7 @@ const wchar_t *const fostlib::exceptions::invalid_ini_line::message() const
 */
 
 
-fostlib::ini_file::ini_file(const fostlib::string &a_fileName)
-: m_fileName(a_fileName) {
+fostlib::ini_file::ini_file(string const &a_fileName) : m_fileName{a_fileName} {
     loadAll();
 }
 
@@ -51,42 +50,39 @@ fostlib::ini_file::ini_file(const fostlib::string &a_fileName)
 void fostlib::ini_file::loadAll() {
     string section, line,
             file(utf::load_file(
-                    coerce<std::wstring>(m_fileName), fostlib::string()));
+                    coerce<fs::path>(m_fileName), fostlib::string{}));
 
-    for (fostlib::string::const_iterator pc(file.begin()); pc != file.end();
-         ++pc) {
-        utf32 c(*pc);
-        if (c == L'\n' || c == L'\r') {
+    for (utf32 const c : file) {
+        if (c == '\n' || c == '\r') {
             fostlib::string read(line);
-            line = line.substr(0, line.find_last_not_of(L" \t") + 1);
+            line = line.substr(0, line.find_last_not_of(" \t") + 1);
             if (line.find_first_not_of(L" \t") != string::npos)
-                line = line.substr(line.find_first_not_of(L" \t"));
-            if (line == L"") {
+                line = line.substr(line.find_first_not_of(" \t"));
+            if (line == "") {
                 // Blank line - ignore it
-            } else if (line[0] == L';') {
+            } else if (line[0] == ';') {
                 // Comment line - ignore it
-            } else if (line[0] == L'[') {
-                section = line.substr(1, line.length() - 2);
-                if (section == L"" || line.find(L']') == string::npos
-                    || section.find_first_of(L"[]") != string::npos)
-                    throw exceptions::invalid_ini_line(L"", read, section);
+            } else if (line[0] == '[') {
+                section = line.substr(1, line.code_points() - 2);
+                if (section == "" || line.find(']') == string::npos
+                    || section.find_first_of("[]") != string::npos)
+                    throw exceptions::invalid_ini_line("", read, section);
             } else {
-                fostlib::string::size_type pos(line.find(L'='));
-                if (pos == string::npos || section == L"")
+                fostlib::string::size_type pos(line.find('='));
+                if (pos == string::npos || section == "")
                     throw exceptions::invalid_ini_line(section, read, line);
                 fostlib::string name(line.substr(0, pos)),
                         value(line.substr(pos + 1));
                 json js = json::parse(value, json());
-                if (!value.empty() && js.isnull())
-                    m_settings.push_back(boost::shared_ptr<setting<string>>(
-                            new setting<string>(
-                                    m_fileName, section, name, value, false)));
-                else
-                    m_settings.push_back(
-                            boost::shared_ptr<setting<json>>(new setting<json>(
-                                    m_fileName, section, name, js, false)));
+                if (!value.empty() && js.isnull()) {
+                    m_settings.push_back(std::make_shared<setting<string>>(
+                            m_fileName, section, name, value, false));
+                } else {
+                    m_settings.push_back(std::make_shared<setting<json>>(
+                            m_fileName, section, name, js, false));
+                }
             }
-            line = L"";
+            line = "";
         } else
             line += c;
     }
