fb56565321c26a2e32c9a8cef99e3b6cfd8e6e12
Semooze <cloudtimewater@gmail.com>
2019-08-06 20:48:03 +0700

    Edit year of license

--- Cpp/fost-core/json-sloppy-parse.cpp
+++ Cpp/fost-core/json-sloppy-parse.cpp
@@ -1,5 +1,5 @@
 /**
-    Copyright 2007-2019, Felspar Co Ltd. <https://support.felspar.com/>
+    Copyright 2019, Felspar Co Ltd. <https://support.felspar.com/>
 
     Distributed under the Boost Software License, Version 1.0.
     See <http://www.boost.org/LICENSE_1_0.txt>
