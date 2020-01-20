d7cfae914feb0da828e2a81b7a878ae23ec8196d
Kirit Sælensminde <kirit@kirit.com>
2020-01-20 11:14:05 +0700

    Fix an order of evaluation problem that surfaced with gcc.

--- Cpp/include/fost/pointers.shared.hpp
+++ Cpp/include/fost/pointers.shared.hpp
@@ -18,18 +18,31 @@ namespace fostlib {
 
     //// ## Conversion between `std` and `boost` `shared_ptr`s
 
+    /**
+     * The code needs to grab the pointer value before calling the
+     * constructor because the evaluation order of arguments is not
+     * specified in C++, meaning that although the `ptr.get()` appears
+     * in the source before the `std::move`, it may be evaluated
+     * afterwards, leading to the wrong pointer value being captured.
+     *
+     * The following body would thus trigger undefined behaviour:
+     * ```cpp
+     * return boost::shared_ptr<T>(
+     *      ptr.get(), [ptr = std::move(ptr)](T *) mutable { ptr.reset(); })
+     *```
+     */
     template<typename T>
     inline boost::shared_ptr<T> to_boost(std::shared_ptr<T> ptr) {
+        T *const p = ptr.get();
         return boost::shared_ptr<T>(
-                ptr.get(),
-                [ptr = std::move(ptr)](T *) mutable { ptr.reset(); });
+                p, [ptr = std::move(ptr)](T *) mutable { ptr.reset(); });
     }
 
     template<typename T>
     inline std::shared_ptr<T> to_std(boost::shared_ptr<T> ptr) {
+        T *const p = ptr.get();
         return std::shared_ptr<T>(
-                ptr.get(),
-                [ptr = std::move(ptr)](T *) mutable { ptr.reset(); });
+                p, [ptr = std::move(ptr)](T *) mutable { ptr.reset(); });
     }
 
 
