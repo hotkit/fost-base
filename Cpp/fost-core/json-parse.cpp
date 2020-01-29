17743fdd067ff6ce2c04382ce8a4e612a03f7b16
Kirit Sælensminde <kirit@kirit.com>
2020-01-30 03:11:55 +0700

    Add a faster path for parsing JSON when there are no Unicode escapes

--- Cpp/fost-core/json-parse.cpp
+++ Cpp/fost-core/json-parse.cpp
@@ -14,17 +14,61 @@
 
 
 namespace {
-    const fostlib::json_parser<
-            f5::cord::const_u32u16_iterator<fostlib::string::const_iterator>>
-            c_json_str_rule;
-    const fostlib::json_parser<
-            f5::cord::const_u32u16_iterator<f5::u8view::const_iterator>>
+    fostlib::json_parser<f5::cord::const_u32u16_iterator<
+            fostlib::string::const_iterator>> const c_json_str_rule;
+    fostlib::json_parser<
+            f5::cord::const_u32u16_iterator<f5::u8view::const_iterator>> const
             c_json_u8v_rule;
-    const fostlib::json_parser<f5::u16view::const_u16_iterator> c_json_u16_rule;
+    fostlib::json_parser<f5::u16view::const_u16_iterator> const c_json_u16_rule;
+
+    fostlib::json_parser<
+            char const *,
+            fostlib::json_string_parser_no_unicode_escapes> const c_json_quick;
+
+    fostlib::json quick_parse(f5::u8view text) {
+        fostlib::json ret{};
+        char const *pos = reinterpret_cast<char const *>(text.memory().begin());
+        char const *const end =
+                reinterpret_cast<char const *>(text.memory().end());
+        try {
+            if (boost::spirit::qi::parse(pos, end, c_json_quick, ret)
+                && pos == end) {
+                return ret;
+            } else {
+            throw fostlib::exceptions::parse_error{
+                    "Whilst quick parsing JSON string",
+                    fostlib::string{std::string{pos, end}}};
+            }
+        } catch (std::runtime_error &e) {
+            throw fostlib::exceptions::parse_error{
+                    e.what(), fostlib::string{std::string{pos, end}}};
+        }
+    }
+
+    fostlib::json quick_parse(f5::u8view text, fostlib::json const &def) {
+        fostlib::json ret{};
+        char const *pos = reinterpret_cast<char const *>(text.memory().begin());
+        char const *const end =
+                reinterpret_cast<char const *>(text.memory().end());
+        try {
+            if (boost::spirit::qi::parse(pos, end, c_json_quick, ret)
+                && pos == end) {
+                return ret;
+            } else {
+                return def;
+            }
+        } catch (std::runtime_error &e) {
+            throw fostlib::exceptions::parse_error{
+                    e.what(), fostlib::string{std::string{pos, end}}};
+        }
+    }
 }
 
 
 fostlib::json fostlib::json::parse(f5::u8view toparse) {
+    if (std::string_view{toparse}.find("\\u") == std::string_view::npos) {
+        return quick_parse(toparse);
+    }
     fostlib::json ret{};
     auto pos = f5::cord::make_u32u16_iterator(toparse.begin(), toparse.end());
     try {
@@ -65,7 +109,10 @@ fostlib::json fostlib::json::parse(f5::u16view s) {
 }
 
 
-fostlib::json fostlib::json::parse(const string &toparse, const json &def) {
+fostlib::json fostlib::json::parse(string const &toparse, json const &def) {
+    if (std::string_view{toparse}.find("\\u") == std::string_view::npos) {
+        return quick_parse(toparse, def);
+    }
     fostlib::json ret{};
     auto pos = f5::cord::make_u32u16_iterator(toparse.begin(), toparse.end());
     try {
--- Examples/fprettyjson/fprettyjson.cpp
+++ Examples/fprettyjson/fprettyjson.cpp
@@ -1,5 +1,5 @@
 /**
-    Copyright 2008-2019 Red Anchor Trading Co. Ltd.
+    Copyright 2008-2020 Red Anchor Trading Co. Ltd.
 
     Distributed under the Boost Software License, Version 1.0.
     See <http://www.boost.org/LICENSE_1_0.txt>
@@ -10,54 +10,58 @@
 #include <fost/main>
 #include <fost/unicode>
 
+#include <fost/parse/json.hpp>
+#include <fost/exception/parse_error.hpp>
+
 
 using namespace fostlib;
 
 
 namespace {
     setting<bool> c_overwrite(
-            L"fprettyjson/fprettyjson.cpp",
-            L"fprettyjson",
-            L"Overwrite original",
+            "fprettyjson/fprettyjson.cpp",
+            "fprettyjson",
+            "Overwrite original",
             false,
             true);
 }
 
 
 FSL_MAIN(
-        L"fprettyjson",
-        L"JSON pretty printer\nCopyright (c) 2008, Felspar Co. Ltd.")
+        "fprettyjson",
+        "JSON pretty printer\nCopyright (c) 2008-2020, Red Anchor Trading Co. "
+        "Ltd.")
 (fostlib::ostream &out, fostlib::arguments &args) {
-    /*
+    /**
         Process the extra command line switches we want
     */
-    args.commandSwitch(L"o", L"fprettyjson", L"Overwrite original");
-    args.commandSwitch(L"s", L"JSON", L"Unparse tab width");
-    /*
+    args.commandSwitch("o", "fprettyjson", "Overwrite original");
+    args.commandSwitch("s", "JSON", "Unparse tab width");
+    /**
         Check that we have an input file name
     */
     if (not args[1]) {
-        out << L"No JSON file to load was specified" << std::endl;
+        out << "No JSON file to load was specified" << std::endl;
         return 1;
     }
-    /*
+    /**
         Load the JSON object
     */
-    json blob(json::parse(
-            utf::load_file(coerce<std::wstring>(args[1].value()).c_str())));
-    /*
+    auto const content = utf::load_file(coerce<fs::path>(args[1].value()));
+    auto const blob{fostlib::json::parse(content)};
+    /**
         If we don't have a 2nd filename and we're not overwriting then output to
        the screen
     */
-    if (not args[2] && not c_overwrite.value())
+    if (not args[2] && not c_overwrite.value()) {
         out << json::unparse(blob, true) << std::endl;
-    else {
-        /*
+    } else {
+        /**
             Otherwise output to the 2nd argument or back to the 1st
         */
         string ofile(args[2].value_or(args[1].value()));
         utf::save_file(coerce<std::wstring>(ofile), json::unparse(blob, true));
-        out << L"JSON saved to " << ofile << std::endl;
+        out << "JSON saved to " << ofile << std::endl;
     }
     return 0;
 }
