af255753aff3696adbbfc36ffda21d1ea244ec93
Kirit Sælensminde <kirit@kirit.com>
2020-05-14 23:09:38 +0700

    Add missing header.

--- Cpp/include/fost/nullable-core.hpp
+++ Cpp/include/fost/nullable-core.hpp
@@ -1,5 +1,5 @@
 /**
-    Copyright 2001-2019 Red Anchor Trading Co. Ltd.
+    Copyright 2001-2020 Red Anchor Trading Co. Ltd.
 
     Distributed under the Boost Software License, Version 1.0.
     See <http://www.boost.org/LICENSE_1_0.txt>
@@ -12,6 +12,8 @@
 
 
 #include <fost/null.hpp>
+
+#include <ostream>
 
 
 namespace fostlib {
