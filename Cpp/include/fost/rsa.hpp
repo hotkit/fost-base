7e003f1032305836d540dd333506a469b56e1bda
SukitOwl <sukit.s@proteus-tech.com>
2020-06-29 11:14:13 +0700

    run clang-format on rsa.hpp

--- Cpp/include/fost/rsa.hpp
+++ Cpp/include/fost/rsa.hpp
@@ -21,21 +21,29 @@ using base64url_encoded = std::string;
 namespace fostlib::rsa {
 
 
-    /// Create RSA private key by base64url_encoded of modulus, public exponent and private exponent
-    CryptoPP::RSA::PrivateKey private_key(base64url_encoded n, base64url_encoded e, base64url_encoded d);
+    /// Create RSA private key by base64url_encoded of modulus, public exponent
+    /// and private exponent
+    CryptoPP::RSA::PrivateKey private_key(
+            base64url_encoded n, base64url_encoded e, base64url_encoded d);
     /// Create RSA public key by base64url_encoded of modulus and public exponent
-    CryptoPP::RSA::PublicKey public_key(base64url_encoded n, base64url_encoded e);
-    
+    CryptoPP::RSA::PublicKey
+            public_key(base64url_encoded n, base64url_encoded e);
+
 
     namespace PKCS1v15_SHA256 {
 
 
         /// Return signature for message signed by private key
-        base64url_encoded sign(base64url_encoded message, CryptoPP::RSA::PrivateKey private_key);
+        base64url_encoded
+                sign(base64url_encoded message,
+                     CryptoPP::RSA::PrivateKey private_key);
         /// Verify signature of message by public key
-        bool validate(base64url_encoded message, base64url_encoded signature, CryptoPP::RSA::PublicKey public_key);
-    
-    
+        bool validate(
+                base64url_encoded message,
+                base64url_encoded signature,
+                CryptoPP::RSA::PublicKey public_key);
+
+
     }
 
 
